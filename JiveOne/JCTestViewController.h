03c8d75e5e3487616be7855957a23ea55e259d49
Daniel George <danielsgeorge@gmail.com>
2014-02-14 15:06:01 -0700

    Unignoring the project.pbxproj file

---
+++ JiveOne/JCTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  JCTestViewController.h
+//  JiveOne
+//
+//  Created by Daniel George on 2/14/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCTestViewController : UITableViewController
+
+@end
