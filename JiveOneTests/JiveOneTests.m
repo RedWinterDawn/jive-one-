69a289d67ebfa673d0bba2ad1114743558f105c6
Daniel George <danielsgeorge@gmail.com>
2014-02-17 13:31:35 -0700

    added storyboard id for start view controller. Working on unit test for retrieve my entity

--- JiveOneTests/JiveOneTests.m
+++ JiveOneTests/JiveOneTests.m
@@ -35,38 +35,49 @@
 }
 
 - (void)testRetrieveAccountInformation{
-    __block NSString *companyUrl;
-    __block NSString *name;
+    NSString *companyUrl;
+    NSString *name;
+    __block NSDictionary *json;
     
     dispatch_semaphore_t sema = dispatch_semaphore_create(0);
     
     JCOsgiClient *client = [JCOsgiClient sharedClient];
+    
+    //    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main_iPhone" bundle:nil];
+    //    JCStartLoginViewController *startVC = [storyboard instantiateViewControllerWithIdentifier:@"JCStartLoginViewController"];
+    //    [startVC loadView];
+    //    [startVC showWebviewForLogin:nil];
+    //    [startVC tokenValidityPassed:nil];
+    
     [client RetrieveMyEntitity:^(id JSON) {
-        name = [JSON objectForKey:@"name"];
-        
-        XCTAssertEqualObjects(name, @"Daniel George", @"Wrong name");
-        companyUrl = [JSON objectForKey:@"company"];
-        
-         dispatch_semaphore_signal(sema);
+        json = JSON;
+        dispatch_semaphore_signal(sema);
         
     } failure:^(NSError *err) {
         XCTFail(@"Retrieve My Company method has failed");
     }];
     
     dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
-    dispatch_semaphore_t sema2 = dispatch_semaphore_create(0);
     
-    [client RetrieveMyCompany:companyUrl:^(id JSON) {
-        
-        XCTAssertEqualObjects([JSON objectForKey:@"company"], @"Jive Communications, Inc", @"Company name doesn't match");
-        
-        dispatch_semaphore_signal(sema2);
-
-        
-    } failure:^(NSError *err) {
-        NSLog(@"%@", err);
-    }];
-    dispatch_semaphore_wait(sema2, DISPATCH_TIME_FOREVER);
+    name = [json objectForKey:@"name"];
+    companyUrl = [json objectForKey:@"company"];
+    XCTAssertEqualObjects(name, @"Daniel George", @"Wrong name");
+    
+    //    dispatch_semaphore_t sema2 = dispatch_semaphore_create(0);
+    //
+    //
+    //    [client RetrieveMyCompany:companyUrl:^(id JSON) {
+    //
+    //        XCTAssertEqualObjects([JSON objectForKey:@"company"], @"Jive Communications, Inc", @"Company name doesn't match");
+    //
+    //        dispatch_semaphore_signal(sema2);
+    //
+    //
+    //    } failure:^(NSError *err) {
+    //        NSLog(@"%@", err);
+    //    }];
+    //    dispatch_semaphore_wait(sema2, DISPATCH_TIME_FOREVER);
+    //    
     
 }
 
