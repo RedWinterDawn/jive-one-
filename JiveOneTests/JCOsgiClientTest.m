c4ba9c2157bdd06cfde057c08564bc3bf5b43db0
Eduardo Gueiros <egueiros@Eduardos-MacBook-Pro.local>
2014-02-19 14:10:56 -0700

    Created many methods that test the API calls of Osgi Client

--- JiveOneTests/JCOsgiClientTest.m
+++ JiveOneTests/JCOsgiClientTest.m
@@ -7,6 +7,8 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "JCOsgiClient.h"
+#import "TRVSMonitor.h"
 
 @interface JCOsgiClientTest : XCTestCase
 
@@ -26,9 +28,176 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testShouldRetrieveClientEntities
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    TRVSMonitor *monitor = [TRVSMonitor monitor];
+    __block NSDictionary* response;
+    
+    [[JCOsgiClient sharedClient] RetrieveClientEntitites:^(id JSON) {
+        response = JSON;
+        [monitor signal];
+    } failure:^(NSError *err) {
+        [monitor signal];
+    }];
+    
+    [monitor wait];
+    
+    XCTAssertNotNil(response, @"Response should not be nil");
+    XCTAssertTrue(([response[@"entries"] count] > 0), @"Response should not have zero entries");
 }
+
+- (void)testShouldRetrieveMyEntity
+{
+    TRVSMonitor *monitor = [TRVSMonitor monitor];
+    __block NSDictionary* response;
+    
+    NSString *expectedEmail = @"egueiros@jive.com";
+    
+    [[JCOsgiClient sharedClient] RetrieveMyEntitity:^(id JSON) {
+        response = JSON;
+        [monitor signal];
+    } failure:^(NSError *err) {
+        [monitor signal];
+    }];
+    
+    [monitor wait];
+    
+    XCTAssertNotNil(response, @"Response should not be nil");
+    
+    NSString *givenEmail = (NSString*)response[@"email"];
+    XCTAssertEqualObjects(givenEmail, expectedEmail, @"Response did not contain valid email");
+}
+
+- (void)testShouldRetrieveMyCompany
+{
+    TRVSMonitor *monitor = [TRVSMonitor monitor];
+    __block NSDictionary* response;
+    
+    NSString *testCompany = @"companies:jive";
+    NSString *expectedCompanyName = @"Jive Communications, Inc.";
+    
+    [[JCOsgiClient sharedClient] RetrieveMyCompany:testCompany :^(id JSON) {
+        response = JSON;
+        [monitor signal];
+    } failure:^(NSError *err) {
+        [monitor signal];
+    }];
+    
+    [monitor wait];
+    
+    XCTAssertNotNil(response, @"Response should not be nil");
+    
+    NSString *givenCompanyName = (NSString*)response[@"name"];   
+    XCTAssertEqualObjects(givenCompanyName, expectedCompanyName, @"Response did not contain correct company name");
+}
+
+- (void)testShouldRetrieveConversations
+{
+    TRVSMonitor *monitor = [TRVSMonitor monitor];
+    __block NSDictionary* response;
+    
+    NSString *expectedChatRoomName = @"The Bar";
+    
+    [[JCOsgiClient sharedClient] RetrieveConversations:^(id JSON) {
+        response = JSON;
+        [monitor signal];
+    } failure:^(NSError *err) {
+        [monitor signal];
+    }];
+     
+    [monitor wait];
+    
+    XCTAssertNotNil(response, @"Response should not be nil");
+    XCTAssertTrue(([response[@"entries"] count] > 0), @"Response should not have zero entries");
+    
+    NSString *givenChatRoomName = (NSString*)response[@"entries"][0][@"name"];
+    XCTAssertEqualObjects(givenChatRoomName, expectedChatRoomName, @"Response did not contain correct chat room name");
+}
+
+- (void)testShouldRequestSocketSession
+{
+    TRVSMonitor *monitor = [TRVSMonitor monitor];
+    __block NSDictionary* response;
+    
+    [[JCOsgiClient sharedClient] RequestSocketSession:^(id JSON) {
+        response = JSON;
+        [monitor signal];
+    } failure:^(NSError *err) {
+        [monitor signal];
+    }];
+    
+    [monitor wait];
+    
+    XCTAssertNotNil(response, @"Response should not be nil");
+    XCTAssert(response[@"urn"], @"Should Contain URN");
+    XCTAssert(response[@"sessionToken"], @"Should Contain Session Token");
+    XCTAssert(response[@"ws"], @"Should Contain WS");
+}
+
+- (void)testShouldSubscribeToSocketEventsWithAuthToken
+{
+    TRVSMonitor *monitor = [TRVSMonitor monitor];
+    __block NSDictionary* response;
+    
+    [[JCOsgiClient sharedClient] RequestSocketSession:^(id JSON) {
+        response = JSON;
+        [monitor signal];
+    } failure:^(NSError *err) {
+        [monitor signal];
+    }];
+    
+    [monitor wait];
+    
+    XCTAssertNotNil(response, @"Response should not be nil");
+    XCTAssert(response[@"urn"], @"Should Contain URN");
+    XCTAssert(response[@"sessionToken"], @"Should Contain Session Token");
+    XCTAssert(response[@"ws"], @"Should Contain WS");
+    
+    NSString *token = response[@"token"];
+    NSDictionary* subscriptions = [NSDictionary dictionaryWithObjectsAndKeys:@"(conversations|permanentrooms|groupconversations|adhocrooms):*:entries:*", @"urn", nil];
+    
+    response = nil;
+    
+    [[JCOsgiClient sharedClient] SubscribeToSocketEventsWithAuthToken:token subscriptions:subscriptions success:^(id JSON) {
+        response = JSON;
+        [monitor signal];
+    } failure:^(NSError *err) {
+        [monitor signal];
+    }];
+    
+    [monitor wait];
+    
+    XCTAssertNotNil(response, @"Response should not be nil");
+    XCTAssert(response[@"urn"], @"Should Contain URN");
+    XCTAssert(response[@"subscriptionUrn"], @"Should Contain Subscription URN");
+    XCTAssert(response[@"session"], @"Should Contain Session");
+    
+}
+
+- (void)testShouldSubmittChatMessageForConversation
+{
+    TRVSMonitor *monitor = [TRVSMonitor monitor];
+    __block NSDictionary* response;
+    
+    NSString *testConversation = @"conversations:1555";
+    NSString *testMessage = @"Automated Test Message";
+    NSString *testEntity = [[JCOmniPresence sharedInstance] me].urn;
+    
+    [[JCOsgiClient sharedClient] SubmitChatMessageForConversation:testConversation message:testMessage withEntity:testEntity success:^(id JSON) {
+        response = JSON;
+        [monitor signal];
+    } failure:^(NSError *err) {
+        [monitor signal];
+    }];
+    
+    [monitor wait];
+    
+    XCTAssertNotNil(response, @"Response should not be nil");
+    
+    NSString *givenMessage = response[@"message"][@"raw"];
+    XCTAssertEqualObjects(givenMessage, testMessage, @"Message received should be same as message posted");
+}
+
+
 
 @end
