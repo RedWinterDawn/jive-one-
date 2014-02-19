27574f036ad28515ba4ce4a9cc65877ff66e1128
Eduardo Gueiros <egueiros@Eduardos-MacBook-Pro.local>
2014-02-19 14:10:10 -0700

    - Created test class for OsgiClient

---
+++ JiveOneTests/JCOsgiClientTest.m
@@ -0,0 +1,34 @@
+//
+//  JCOsgiClientTest.m
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 2/19/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JCOsgiClientTest : XCTestCase
+
+@end
+
+@implementation JCOsgiClientTest
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
