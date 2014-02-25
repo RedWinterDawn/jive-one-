ec0f8b79fb18c81e39a470ceb7575157110d79d0
Eduardo Gueiros <egueiros@Eduardos-MacBook-Pro.local>
2014-02-25 15:07:31 -0700

    Created JCPersonCell class, modified storyboard to use new class.

---
+++ JiveOne/JCPersonCell.h
@@ -0,0 +1,17 @@
+//
+//  JCPersonCell.h
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 2/25/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCPersonCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *personNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *personDetailLabel;
+@property (weak, nonatomic) IBOutlet UILabel *personPresenceLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *personPicture;
+@end
