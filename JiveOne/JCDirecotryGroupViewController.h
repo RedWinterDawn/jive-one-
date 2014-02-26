e663bf4d3fdc567b8c6b3f4ffdef8e5294a3c61f
Ethan Parker <eparker@getjive.com>
2014-02-26 10:53:06 -0700

    US505 : TA1860 - << This is the info from Rally! We'd like to start tagging our commits.  >> - added the JCDirectoryGroupviewController class (.h+.m) and created a segue from DirectoryViewController

---
+++ JiveOne/JCDirecotryGroupViewController.h
@@ -0,0 +1,13 @@
+//
+//  JCDirecotryGroupViewController.h
+//  JiveOne
+//
+//  Created by Ethan Parker on 2/25/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCDirecotryGroupViewController : UITableViewController
+
+@end
