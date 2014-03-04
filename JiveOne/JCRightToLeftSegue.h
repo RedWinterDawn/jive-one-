29e93ac01da56f6505e6eb83628032f877167c47
Eduardo Gueiros <egueiros@Eduardos-MacBook-Pro.local>
2014-03-03 18:37:40 -0700

    created custom segue right->left

---
+++ JiveOne/JCRightToLeftSegue.h
@@ -0,0 +1,13 @@
+//
+//  JCRightToLeftSegue.h
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 3/3/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCRightToLeftSegue : UIStoryboardSegue
+
+@end
