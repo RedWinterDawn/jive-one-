45a59c179e6d0b3070cd6c6e1554b47f491c7ad2
Eduardo Gueiros <egueiros@jive.com>
2014-03-10 23:17:45 -0600

    Created categories to put classes CRUD operations and other custom operations in.

---
+++ JiveOne/Conversation+Custom.m
@@ -0,0 +1,13 @@
+//
+//  Conversation+Custom.m
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 3/10/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import "Conversation+Custom.h"
+
+@implementation Conversation (Custom)
+
+@end
---
+++ JiveOne/ConversationEntry+Custom.h
@@ -0,0 +1,13 @@
+//
+//  ConversationEntry+Custom.h
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 3/10/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import "ConversationEntry.h"
+
+@interface ConversationEntry (Custom)
+
+@end
