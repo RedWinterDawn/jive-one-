74a2fa0a6446ea54531e6246a0bad48cd6326003
Eduardo Gueiros <egueiros@jive.com>
2014-03-12 15:04:42 -0600

    Presence view programmatically draws our 'presence' circles. The view updates based on KVO of the presence value changes in CoreDate

---
+++ JiveOne/JCPresenceView.h
@@ -0,0 +1,13 @@
+//
+//  JCPresenceView.h
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 3/11/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCPresenceView : UIView
+
+@end
---
+++ JiveOne/JCPresenceView.m
@@ -0,0 +1,31 @@
+//
+//  JCPresenceView.m
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 3/11/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import "JCPresenceView.h"
+
+@implementation JCPresenceView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
