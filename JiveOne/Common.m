b38a06aaf8796f408473b4a881676f9e85f5e231
Eduardo Gueiros <egueiros@jive.com>
2014-03-12 19:22:07 -0600

    Added common class for common operations in the application

---
+++ JiveOne/Common.m
@@ -0,0 +1,13 @@
+//
+//  Common.m
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 3/12/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import "Common.h"
+
+@implementation Common
+
+@end
