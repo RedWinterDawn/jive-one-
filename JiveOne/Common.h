a8f0a047e8ba4dd4379cae2102572ccc7276f398
Eduardo Gueiros <egueiros@jive.com>
2014-03-12 19:22:40 -0600

    Added some Date helper methods to the Commons class

--- JiveOne/Common.h
+++ JiveOne/Common.h
@@ -8,6 +8,25 @@
 
 #import <Foundation/Foundation.h>
 
+#define D_MINUTE	60
+#define D_HOUR		3600
+#define D_DAY		86400
+#define D_WEEK		604800
+#define D_YEAR		31556926
+
 @interface Common : NSObject
+
++(NSString *) dateFromTimestamp:(NSNumber *)timestamp;
++ (BOOL) isEqualToDateIgnoringTime: (NSDate *) aDate;
++ (BOOL) isEarlierThanDate: (NSDate *) aDate;
++ (BOOL) isLaterThanDate: (NSDate *) aDate;
+// Retrieving intervals
++ (NSInteger) minutesAfterDate: (NSDate *) aDate;
++ (NSInteger) minutesBeforeDate: (NSDate *) aDate;
++ (NSInteger) hoursAfterDate: (NSDate *) aDate;
++ (NSInteger) hoursBeforeDate: (NSDate *) aDate;
++ (NSInteger) daysAfterDate: (NSDate *) aDate;
++ (NSInteger) daysBeforeDate: (NSDate *) aDate;
++ (NSInteger)distanceInDaysToDate:(NSDate *)anotherDate;
 
 @end
--- JiveOne/Common.m
+++ JiveOne/Common.m
@@ -10,4 +10,95 @@
 
 @implementation Common
 
+#define DATE_COMPONENTS (NSYearCalendarUnit| NSMonthCalendarUnit | NSDayCalendarUnit | NSWeekCalendarUnit |  NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit | NSWeekdayCalendarUnit | NSWeekdayOrdinalCalendarUnit)
+#define CURRENT_CALENDAR [NSCalendar currentCalendar]
+
++(NSString *) dateFromTimestamp:(NSNumber *)timestamp
+{
+    NSTimeInterval timeInterval = [timestamp longLongValue]/1000;
+    NSDate *date = [[NSDate alloc]initWithTimeIntervalSince1970: timeInterval];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.timeZone = [NSTimeZone defaultTimeZone];
+    
+    int days = [self daysBeforeDate:date];
+    
+    if (days == 0) {
+        [formatter setDateFormat:@"HH:mm a"];
+        NSString* hour = [formatter stringFromDate:date];
+        return [NSString stringWithFormat:@"%@ %@", NSLocalizedString(@"Today", @"Today"), hour];
+    } else if (days == 1) {
+        [formatter setDateFormat:@"HH:mm a"];
+        NSString* hour = [formatter stringFromDate:date];
+        return [NSString stringWithFormat:@"%@ %@", NSLocalizedString(@"Yesterday", @"Yesterday"), hour];
+    } else if (days > 1 && days < 6) {
+        [formatter setDateFormat:@"EEEE HH:mm a"];
+        return [formatter stringFromDate:date];
+    }
+        
+    [formatter setDateFormat:@"MM/dd/yyyy hh:mm a"];
+    NSTimeZone *timezone = [NSTimeZone defaultTimeZone];
+    formatter.timeZone = timezone;
+    return [formatter stringFromDate:date];
+}
+
++ (BOOL) isEqualToDateIgnoringTime: (NSDate *) aDate
+{
+	NSDateComponents *components1 = [CURRENT_CALENDAR components:DATE_COMPONENTS fromDate:[NSDate date]];
+	NSDateComponents *components2 = [CURRENT_CALENDAR components:DATE_COMPONENTS fromDate:aDate];
+	return ((components1.year == components2.year) &&
+			(components1.month == components2.month) &&
+			(components1.day == components2.day));
+}
+
++ (BOOL) isEarlierThanDate: (NSDate *) aDate
+{
+	return ([[NSDate date] compare:aDate] == NSOrderedAscending);
+}
+
++ (BOOL) isLaterThanDate: (NSDate *) aDate
+{
+	return ([[NSDate date] compare:aDate] == NSOrderedDescending);
+}
+
+#pragma mark Retrieving Intervals
+
++ (NSInteger) minutesAfterDate: (NSDate *) aDate
+{
+	NSTimeInterval ti = [[NSDate date] timeIntervalSinceDate:aDate];
+	return (NSInteger) (ti / D_MINUTE);
+}
+
++ (NSInteger) minutesBeforeDate: (NSDate *) aDate
+{
+	NSTimeInterval ti = [aDate timeIntervalSinceDate:[NSDate date]];
+	return (NSInteger) (ti / D_MINUTE);
+}
+
++ (NSInteger) hoursAfterDate: (NSDate *) aDate
+{
+	NSTimeInterval ti = [[NSDate date]  timeIntervalSinceDate:aDate];
+	return (NSInteger) (ti / D_HOUR);
+}
+
++ (NSInteger) hoursBeforeDate: (NSDate *) aDate
+{
+	NSTimeInterval ti = [aDate timeIntervalSinceDate:[NSDate date]];
+	return (NSInteger) (ti / D_HOUR);
+}
+
++ (NSInteger) daysAfterDate: (NSDate *) aDate
+{
+	NSTimeInterval ti = [[NSDate date]  timeIntervalSinceDate:aDate];
+	return (NSInteger) (ti / D_DAY);
+}
+
++ (NSInteger) daysBeforeDate: (NSDate *) aDate
+{
+	NSTimeInterval ti = [aDate timeIntervalSinceDate:[NSDate date]];
+	return (NSInteger) (ti / D_DAY);
+}
+
+
+
+
 @end
