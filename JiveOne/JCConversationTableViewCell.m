0c55a471349f2f8acea841a2bdd034c572c5ec00
Eduardo Gueiros <egueiros@jive.com>
2014-03-13 20:43:26 -0600

    US1611 : TA3135 fix for app breaking on scroll.

--- JiveOne/JCConversationTableViewCell.m
+++ JiveOne/JCConversationTableViewCell.m
@@ -29,54 +29,62 @@
 
 -(void)setConversation:(Conversation *)conversation
 {
-    [self removeObservers];
+    //[self removeObservers];
     if ([conversation isKindOfClass:[Conversation class]]) {
-        _conversation = conversation;
         
-        if (!_conversation.isGroup) {
-            NSArray *entitiesArray = (NSArray*)_conversation.entities;
-            NSString *firstEntity = nil;
-            
-            for (NSString* entity in entitiesArray) {
-                if (![entity isEqualToString:[[JCOmniPresence sharedInstance] me].urn]) {
-                    firstEntity = entity;
-                }
+        _conversation = conversation;
+        // add observer to the lastmodified property of the conversation
+        [_conversation addObserver:self forKeyPath:kLastMofiedKeyPathForConversation options:NSKeyValueObservingOptionNew context:NULL];
+        
+        
+        // get first entity that is not me: for 1:1 conversations, it will be the other person. For group, it can be anyone.
+        NSArray *entitiesArray = (NSArray*)_conversation.entities;
+        NSString *firstEntity = nil;
+        for (NSString* entity in entitiesArray) {
+            if (![entity isEqualToString:[[JCOmniPresence sharedInstance] me].urn]) {
+                firstEntity = entity;
+                break;
             }
+        }
+        
+        ClientEntities * person = [[JCOmniPresence sharedInstance] entityByEntityId:firstEntity];
+        if (person) {
+            _person = person;
+            // add observer to the person presence, even if it's a group, just so our app don't break.
+            [_person addObserver:self forKeyPath:kPresenceKeyPathForClientEntity options:NSKeyValueObservingOptionNew context:NULL];
             
-            ClientEntities * person = [[JCOmniPresence sharedInstance] entityByEntityId:firstEntity];
-            if (person) {
-                _person = person;
-                self.conversationTitle.text = _person.firstLastName;
-                self.presenceView.presenceType = (JCPresenceType)[person.entityPresence.interactions[@"chat"][@"code"] integerValue];
-                [_person addObserver:self forKeyPath:kPresenceKeyPathForClientEntity options:NSKeyValueObservingOptionNew context:NULL];
-                self.presenceView.hidden = NO;
-            }
-            else {
+            // set cell title
+            if ([_conversation.isGroup boolValue]) {
+                self.conversationTitle.text = _conversation.name;
+                
+                // if it's a group, we don't need to set presence and we can hide it
                 self.presenceView.hidden = YES;
             }
-        }
-        else
-        {
-            self.conversationTitle.text = [NSString stringWithFormat:@"%@", _conversation.name];
-            self.presenceView.hidden = YES;
+            else {
+                self.conversationTitle.text = person.firstLastName;
+                
+                // if it's a person we want to set presence;
+                self.presenceView.presenceType = (JCPresenceType)[person.entityPresence.interactions[@"chat"][@"code"] integerValue];
+                self.presenceView.hidden = NO;
+            }
         }
         
+        // set the conversastion time label
         self.conversationTime.text = [Common dateFromTimestamp:_conversation.lastModified];
         
+        // grab the last entry for the conversation and set in the snippet lable
         NSPredicate *predicate = [NSPredicate predicateWithFormat:@"conversationId ==[c] %@", _conversation.conversationId];
         ConversationEntry *lastEntry = [ConversationEntry MR_findFirstWithPredicate:predicate sortedBy:@"lastModified" ascending:NO];
         if (lastEntry) {
             self.conversationSnippet.text = lastEntry.message[@"raw"];
         }
-        
-        [_conversation addObserver:self forKeyPath:kLastMofiedKeyPathForConversation options:NSKeyValueObservingOptionNew context:NULL];
     }
 }
 
 -(void)prepareForReuse
 {
     [super prepareForReuse];
-    //[self removeObservers];
+    [self removeObservers];
 }
 
 -(void)removeObservers
