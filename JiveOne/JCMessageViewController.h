7dfe90947536b6fdbb21a950b50c520672aefd7e
Eduardo Gueiros <egueiros@jive.com>
2014-03-18 11:04:31 -0600

    Added new files that are part of message chat refactoring.

---
+++ JiveOne/JCMessageViewController.h
@@ -0,0 +1,13 @@
+//
+//  JCMessageViewController.h
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 3/14/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCMessageViewController : UITableViewController
+
+@end
