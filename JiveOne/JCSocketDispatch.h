1a594ea3bb9fb50f8f62865f2875007262de15ff
Eduardo Gueiros <egueiros@jive.com>
2014-04-09 14:27:07 -0600

    US2294 made rest of the methods publicly available.

--- JiveOne/JCSocketDispatch.h
+++ JiveOne/JCSocketDispatch.h
@@ -20,6 +20,11 @@
 
 - (void)closeSocket;
 
+#pragma mark - Badge Updates
+- (void)incrementBadgeCountForConversation:(NSString *)conversationId;
+- (void)incrementBadgeCountForVoicemail;
+- (void)decrementBadgeCountForConversation:(NSString *)conversationId;
+- (void)decrementBadgeCountForVoicemail;
 - (void)clearBadgeCountForVoicemail;
 
 @end
