24b77804204e18c25efa87a90cef6ad3fa718f3e
Eduardo Gueiros <egueiros@jive.com>
2014-04-16 10:34:53 -0600

    project cleanup

---
+++ JiveOne/JCStatusViewController.h
@@ -0,0 +1,13 @@
+//
+//  JCStatusViewController.h
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 4/16/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCStatusViewController : UIViewController
+
+@end
