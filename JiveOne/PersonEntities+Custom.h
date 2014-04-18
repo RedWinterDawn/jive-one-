fcbfa512bfc9ea45917e8eb144cd3db6a9adc08b
Eduardo Gueiros <egueiros@jive.com>
2014-04-18 17:26:24 -0600

    clean up

---
+++ JiveOne/PersonEntities+Custom.h
@@ -0,0 +1,13 @@
+//
+//  PersonEntities+Custom.h
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 4/18/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import "PersonEntities.h"
+
+@interface PersonEntities (Custom)
+
+@end
---
+++ JiveOne/PersonEntities+Custom.m
@@ -0,0 +1,13 @@
+//
+//  PersonEntities+Custom.m
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 4/18/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import "PersonEntities+Custom.h"
+
+@implementation PersonEntities (Custom)
+
+@end
