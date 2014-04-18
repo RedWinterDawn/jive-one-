73f7dbf4e422e947ca7fc08f74d226d5ade2a44d
Eduardo Gueiros <egueiros@jive.com>
2014-04-18 14:20:10 -0600

    DE252 Refactored some elements of voicemail

---
+++ JiveOne/JCVoiceTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JCVoiceTableViewController.h
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 4/17/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCVoiceTableViewController : UITableViewController
+
+@end
