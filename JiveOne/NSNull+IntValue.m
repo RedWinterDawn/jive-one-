f213205d391df41c50915105f05ab1933de4bbe0
Eduardo Gueiros <egueiros@jive.com>
2014-04-28 11:01:22 -0600

    Added two new entities to keep track of the ETags for message replaying.

---
+++ JiveOne/NSNull+IntValue.m
@@ -0,0 +1,13 @@
+//
+//  NSNull+IntValue.m
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 4/28/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import "NSNull+IntValue.h"
+
+@implementation NSNull (IntValue)
+
+@end
---
+++ JiveOne/VoicemailETag.m
@@ -0,0 +1,16 @@
+//
+//  VoicemailETag.m
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 4/28/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import "VoicemailETag.h"
+
+
+@implementation VoicemailETag
+
+@dynamic etag;
+
+@end
