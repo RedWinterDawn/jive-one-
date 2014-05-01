beb1690f17484a7dda17aa77995dd74158217820
Eduardo Gueiros <egueiros@jive.com>
2014-05-01 15:21:33 -0600

    [US2897] Helper classes for OAuth Grant Password

---
+++ JiveOne/NSDictionary+QueryString.h
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+QueryString.h
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 5/1/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (QueryString)
+
+@end
---
+++ JiveOne/NSString+URLEncoding.m
@@ -0,0 +1,13 @@
+//
+//  NSString+URLEncoding.m
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 5/1/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import "NSString+URLEncoding.h"
+
+@implementation NSString (URLEncoding)
+
+@end
