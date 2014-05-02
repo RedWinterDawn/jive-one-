63b80d6a2bbdada992e228f3f7d58ec18da0d1d1
Daniel George <danielsgeorge@gmail.com>
2014-05-01 21:56:31 -0600

    added some accessibility handles and committing a new automated test for login

---
+++ JiveOneTests/AutomatedTests/login.js
@@ -0,0 +1,13 @@
+var target = UIATarget.localTarget();
+var app = target.frontMostApp();
+var window = app.mainWindow();
+
+window.logElementTree();
+
+window.textFields()["emailTextField"].setValue("jivetesting12@gmail.com");
+window.secureTextFields()["passwordTextField"].setValue("testing13");
+window.logElementTree();
+app.keyboard().elements()["Go"].tap();
+
+UIATarget.localTarget().pushTimeout(20);
+window.navigationBar().name()["CompanyDirectory"];
