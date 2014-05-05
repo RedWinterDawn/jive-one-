6258b5fb67336bc99394d99d85bd58597cf2da96
Daniel George <danielsgeorge@gmail.com>
2014-05-05 16:49:24 -0600

    US2923: edited Common.js to automatically check if the user is logged before executing tests. if they are not logged in, it will also automatically log in. finished Receive a chat test (TC5)

--- JiveOneTests/AutomatedTests/Common.js
+++ JiveOneTests/AutomatedTests/Common.js
@@ -1,45 +1,75 @@
-
 function test(title, f, options) {
-  if (options == null) {
-    options = {
-      logTree: true
-    };
-  }
-  target = UIATarget.localTarget();
-  application = target.frontMostApp();
-  UIALogger.logStart(title);
-  try {
-    f(target, application);
-    UIALogger.logPass(title);
-  }
-  catch (e) {
-    UIALogger.logError(e);
-    if (options.logTree) target.logElementTree();
-    UIALogger.logFail(title);
-  }
+    if (options == null) {
+        options = {
+            logTree: true
+        };
+    }
+    target = UIATarget.localTarget();
+    application = target.frontMostApp();
+    UIALogger.logStart(title);
+
+    try {
+        //login check
+        loginCheck(application);
+
+        f(target, application);
+        UIALogger.logPass(title);
+    } catch (e) {
+        UIALogger.logError(e);
+        if (options.logTree) target.logElementTree();
+        UIALogger.logFail(title);
+    }
 };
 
+function loginCheck(app) {
+
+        UIALogger.logDebug("test if we are logged out...");
+        if (app.mainWindow().textFields()[0].name() == "emailTextField") {
+            var currentWindow = app.mainWindow();
+
+            //login
+            currentWindow.textFields()["emailTextField"].setValue("jivetesting10@gmail.com");
+            currentWindow.secureTextFields()["passwordTextField"].setValue("testing12");
+            app.keyboard().elements()["Go"].tap();
+
+            //wait for login
+            UIATarget.localTarget().pushTimeout(200);
+            currentWindow.navigationBar().name()["People"];
+            UIATarget.localTarget().popTimeout();
+
+            UIATarget.localTarget().delay(2);
+        }else{
+          UIALogger.logDebug("Not logged out");  
+        }
+        // UIALogger.logDebug("end if");
+ 
+
+}
+
+
 function assertEquals(expected, received, message) {
-  if (received != expected) {
-    if (! message) message = "Expected " + expected + " but received " + received;
-    throw message;
-  }
+    if (received != expected) {
+        if (!message) message = "Expected " + expected + " but received " + received;
+        throw message;
+    }
 }
-  
+
 function assertTrue(expression, message) {
-  if (! expression) {
-    if (! message) message = "Assertion failed";
-    throw message;
-  }
+    if (!expression) {
+        if (!message) message = "Assertion failed";
+        throw message;
+    }
 }
-  
+
 function assertFalse(expression, message) {
-  assertTrue(! expression, message);
+    assertTrue(!expression, message);
 }
-  
+
 function assertNotNull(thingie, message) {
-  if (thingie == null || thingie.toString() == "[object UIAElementNil]") {
-    if (message == null) message = "Expected not null object";
-    throw message;
-  }
+    if (thingie == null || thingie.toString() == "[object UIAElementNil]") {
+        if (message == null) message = "Expected not null object";
+        throw message;
+    }
 }
+
+String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
--- JiveOneTests/AutomatedTests/Receive a chat.js
+++ JiveOneTests/AutomatedTests/Receive a chat.js
@@ -1,42 +1,76 @@
 #import "Common.js"
-#import "jquery.min.js"
 
 test("Receive a chat", function(target, app){
+
 
 	var w = app.mainWindow();
 
 	//navigate to chat window
-	var conversations = mainWindow.tabBar().buttons()[2];
-	conversations.tap();
-	target.delay(1);
-	assertEquals("Conversations", app.mainWindow().navigationBar().name(), "Title should be 'Conversations'");
-
-	//select a person to chat
-	app.mainWindow().logElementTree();
-	w = app.mainWindow();
-	var cell = w.tableViews()[0].cells["The Bar"];
-	cell.tap();
-	target.delay(1);
-
+	navigateToConversationWindow(app);
 
 	//send a message
-	app.Keyboard().keys("Your message");
-	app.mainWindow().logElementTree();
-	app.keyboard().elements()["Send"].tap();
-
-	//verify that the recipient received the message
-		//log onto the server using jivetesting11's credentials
-	$.get(
-    	"my.jive.com",
-    	{username : "jivetesting11", password : 'testing12'},
-    	function(data) {
-       		alert('page content: ' + data);
-    }
-);
-	
-
-	more.tap();
+	w = app.mainWindow();
 	target.delay(1);
-	assertEquals("Account", app.mainWindow().navigationBar().name(), "Title should be 'Account'");
+	var beforeCount = w.tableViews()[0].cells().length;
+
+	var uniqueMessage = "my message " + new Date().toString();
+	w.images()[0].textViews()[0].tap();
+	app.keyboard().typeString(uniqueMessage);
+	w.images()[0].buttons()["Send"].tap();
+
+	var afterCount = w.tableViews()[0].cells().length;
+
+	assertTrue((beforeCount+1)==afterCount, "There should have been "+(beforeCount+1)+" but there were "+ afterCount);
+
+	//give it 3 seconds to actually post the message
+	target.delay(3)
+
+	//verify that the recipient received the message by logging out of jivetesting11 and into jivetesting10, then checking messages
+	//go to logout page
+	w.navigationBar().leftButton().tap();
+	app.mainWindow().tabBar().buttons()[3].tap();
+	target.logElementTree();
+	
+	var recipient = app.mainWindow().tableViews()[0].cells()[0].name();
+	UIALogger.logDebug("logged in as:" + recipient);
+	if(recipient.contains("jivetesting10@gmail.com"))
+	{
+		recipient = "jivetesting11@gmail.com";
+	}else{
+		recipient = "jivetesting10@gmail.com";
+	}
+
+	app.mainWindow().navigationBar().buttons()["Logout"].tap();
+	UIALogger.logDebug("will log in as: "+ recipient);
+
+	app.mainWindow().textFields()["emailTextField"].setValue(recipient);
+	app.mainWindow().secureTextFields()["passwordTextField"].setValue("testing12");
+	app.keyboard().elements()["Go"].tap();
+
+	//wait for login
+	target.pushTimeout(200);
+	app.mainWindow().navigationBar().name()["People"];
+	target.popTimeout();
+
+	//navigate to conversations tab
+	navigateToConversationWindow(app);
+
+	//assert that message we sent is in this conversation
+	target.delay(2);
+
+	assertTrue(app.mainWindow().tableViews()[0].cells()[uniqueMessage], "Table View did not contain most recently sent message");
+	
 	
 });
+
+
+function navigateToConversationWindow(app){
+	//navigate to chat window
+	app.mainWindow().tabBar().buttons()[2].tap();
+	UIATarget.localTarget().delay(1);
+	assertEquals("Conversations", app.mainWindow().navigationBar().name(), "Title should be 'Conversations'");
+	//TODO: ? pull down refresh?
+
+	//select a person to chat
+	app.mainWindow().tableViews()[0].cells()[0].tap();
+}
