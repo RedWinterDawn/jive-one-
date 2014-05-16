d2bccdadbc292c9cdfc7a4a5ecf25d8eb815c209
Doug <doug@jive.com>
2014-05-15 20:05:10 -0600

    Tweaking VM UI

--- JiveOne/JCPopoverSlider.m
+++ JiveOne/JCPopoverSlider.m
@@ -8,10 +8,6 @@
 
 #import "JCPopoverSlider.h"
 @interface JCPopoverSlider()
-@property (strong,nonatomic) UIImage* sliderImage;
-@property (nonatomic, strong) NSString *sliderText;
-@property (nonatomic, strong) UIFont *sliderFont;
-
 
 @end
 
@@ -27,19 +23,77 @@
     return self;
 }
 
+//The default initWithFrame: method is not invoked when creating an object from a .xib file. Instead, it is this method that is invoked - initWithCoder:.
+-(id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self constructSlider];
+    }
+    return self;
+}
+
 #pragma mark - Helper methods
 -(void)constructSlider {
     self.continuous = YES;
-    [self addTarget:self action:@selector(sliderValueChanged:) forControlEvents:UIControlEventValueChanged];
-    _popupView = [[JCPopoverView alloc] initWithFrame:CGRectZero];
-    _popupView.backgroundColor = [UIColor clearColor];
-    _popupView.alpha = 0.0;
-    [self addSubview:_popupView];
-    [[JCPopoverSlider appearance] setThumbImage:sliderImage([self formatSeconds:self.value]) forState:UIControlStateNormal];
-//    [self addSubview: self.sliderTextLabel];
-    [self.sliderTextLabel setNeedsDisplay];
+
+    [[JCPopoverSlider appearance] setThumbImage: sliderImage([self formatSeconds:self.value]) forState:UIControlStateNormal];
+    [[JCPopoverSlider appearance] setThumbImage: justASliderBox() forState:UIControlStateHighlighted];
+    
+    // Create the callbacks for touch, move, and release
+    [self addTarget:self action:@selector(startDrag:) forControlEvents:UIControlEventTouchDown];
+    [self addTarget:self action:@selector(updateThumb) forControlEvents:UIControlEventValueChanged];
+    [self addTarget:self action:@selector(endDrag:) forControlEvents:UIControlEventTouchUpInside | UIControlEventTouchUpOutside];
+}
+-(JCPopoverView*)popupView
+{
+    if (!_popupView) {
+        _popupView = [[JCPopoverView alloc] initWithFrame:CGRectZero];
+        _popupView.backgroundColor = [UIColor clearColor];
+        _popupView.alpha = 0.0;
+        [self addSubview:_popupView];
+    }
+    return _popupView;
 }
 
+- (void)updateThumb{
+//    NSLog(@"slider value = %f", sender.value);
+    UIImage *customimg = sliderImage([self formatSeconds:self.value]);
+	[self setThumbImage: customimg forState: UIControlStateHighlighted];
+}
+
+-(void)fadePopupViewInAndOut:(BOOL)aFadeIn {
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:0.5];
+    if (aFadeIn) {
+        _popupView.alpha = 1.0;
+    } else {
+        _popupView.alpha = 0.0;
+    }
+    [UIView commitAnimations];
+}
+
+-(void)positionAndUpdatePopupView {
+    CGRect zeThumbRect = self.thumbRect;
+    CGRect popupRect = CGRectOffset(zeThumbRect, -(zeThumbRect.size.width/2), -floor(zeThumbRect.size.height * 2.6));
+    self.popupView.frame = CGRectInset(popupRect, -20, -10);
+    self.popupView.value = self.value;
+}
+
+/** Time formatting helper fn: N seconds => MM:SS */
+-(NSString *)formatSeconds:(NSTimeInterval)seconds {
+    NSInteger minutes = (NSInteger)(seconds/60.);
+    NSInteger remainingSeconds = (NSInteger)seconds % 60;
+    return [NSString stringWithFormat:@"%.2ld:%.2ld",(long)minutes,(long)remainingSeconds];
+}
+
+#pragma mark - Property accessors
+-(CGRect)thumbRect {
+    CGRect trackRect = [self trackRectForBounds:self.bounds];
+    CGRect thumbR = [self thumbRectForBounds:self.bounds trackRect:trackRect value:self.value];
+    return thumbR;
+}
+
+#pragma mark - ThumbImages
 UIImage *sliderImage(NSString* text)
 {
     UIGraphicsBeginImageContextWithOptions(CGSizeMake(32, 16), FALSE, [[UIScreen mainScreen]scale]);
@@ -59,10 +113,10 @@ UIImage *sliderImage(NSString* text)
     NSMutableParagraphStyle *paragrapStyle = [[NSMutableParagraphStyle alloc] init];
     paragrapStyle.alignment = NSTextAlignmentCenter;
     NSDictionary *att = @{
-      NSFontAttributeName : font,
-      NSForegroundColorAttributeName : [UIColor whiteColor],
-      NSParagraphStyleAttributeName : paragrapStyle
-    };
+                          NSFontAttributeName : font,
+                          NSForegroundColorAttributeName : [UIColor whiteColor],
+                          NSParagraphStyleAttributeName : paragrapStyle
+                          };
     [text drawInRect:rect withAttributes:att];
     //get the image from the context
     UIImage* sliderImage = UIGraphicsGetImageFromCurrentImageContext();
@@ -72,75 +126,40 @@ UIImage *sliderImage(NSString* text)
     return sliderImage;
 }
 
-- (IBAction)sliderValueChanged:(UISlider *)sender {
-    NSLog(@"slider value = %f", sender.value);
-//    [self positionAndUpdateSlider];
-    UIImage *customimg = sliderImage([self formatSeconds:self.value]);
-	[self setThumbImage: customimg forState: UIControlStateHighlighted];
-}
-
--(UIImageView*)sliderView{
-    if (!_sliderView) {
-        _sliderView = [[UIImageView alloc]initWithImage:_sliderImage];
-    }
-    return _sliderView;
-}
-
-
-
-//The default initWithFrame: method is not invoked when creating an object from a .xib file. Instead, it is this method that is invoked - initWithCoder:.
--(id)initWithCoder:(NSCoder *)aDecoder {
-    self = [super initWithCoder:aDecoder];
-    if (self) {
-        [self constructSlider];
-    }
-    return self;
-}
-
--(void)fadePopupViewInAndOut:(BOOL)aFadeIn {
-    [UIView beginAnimations:nil context:NULL];
-    [UIView setAnimationDuration:0.5];
-    if (aFadeIn) {
-        _popupView.alpha = 1.0;
-    } else {
-        _popupView.alpha = 0.0;
-    }
-    [UIView commitAnimations];
-}
-
--(void)positionAndUpdateSlider
+UIImage *justASliderBox()
 {
-    _sliderTextLabel.frame = self.thumbRect;
+    UIGraphicsBeginImageContextWithOptions(CGSizeMake(20, 20), FALSE, [[UIScreen mainScreen]scale]);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    //// Color Declarations
+    UIColor* color = [UIColor colorWithRed: 0.275 green: 0.396 blue: 0.843 alpha: 1];
+    //// Rectangle Drawing
+    CGRect rect = CGRectMake(0, 0, 20, 20);
+    CGContextAddRect(context, rect);
+    CGContextSetStrokeColorWithColor(context, color.CGColor);
+    CGContextSetFillColorWithColor(context, color.CGColor);
+    CGContextDrawPath(context, kCGPathFillStroke);
     
-//    _sliderTextLabel.text = [self formatSeconds:self.value];
-    self.sliderText = [self formatSeconds:self.value];
-    self.sliderTextLabel.text = self.sliderText;
-    [self bringSubviewToFront:self.sliderTextLabel];
-    [self.sliderTextLabel setNeedsDisplay];
+    //get the image from the context
+    UIImage* sliderImage = UIGraphicsGetImageFromCurrentImageContext();
+    [sliderImage drawInRect:rect];
+    UIGraphicsEndImageContext();
+    
+    return sliderImage;
 }
--(void)positionAndUpdatePopupView {
-    CGRect zeThumbRect = self.thumbRect;
-    CGRect popupRect = CGRectOffset(zeThumbRect, -(zeThumbRect.size.width/2), -floor(zeThumbRect.size.height * 1.4));
-    _popupView.frame = CGRectInset(popupRect, -20, -10);
-    _popupView.value = self.value;
-}
-
-/** Time formatting helper fn: N seconds => MM:SS */
--(NSString *)formatSeconds:(NSTimeInterval)seconds {
-    NSInteger minutes = (NSInteger)(seconds/60.);
-    NSInteger remainingSeconds = (NSInteger)seconds % 60;
-    return [NSString stringWithFormat:@"%.2ld:%.2ld",(long)minutes,(long)remainingSeconds];
-}
-
-#pragma mark - Property accessors
--(CGRect)thumbRect {
-    CGRect trackRect = [self trackRectForBounds:self.bounds];
-    CGRect thumbR = [self thumbRectForBounds:self.bounds trackRect:trackRect value:self.value];
-    return thumbR;
-}
-
 
 #pragma mark - UIControl touch event tracking
+// Expand the slider to accommodate the bigger thumb
+- (void)startDrag:(UISlider *)aSlider
+{
+	self.frame = CGRectInset(self.frame, 0.0f, -30.0f);
+}
+
+// At release, shrink the frame back to normal
+- (void)endDrag:(UISlider *)aSlider
+{
+    self.frame = CGRectInset(self.frame, 0.0f, 30.0f);
+}
+
 -(BOOL)beginTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event {
     // Fade in and update the popup view
     CGPoint touchPoint = [touch locationInView:self];
