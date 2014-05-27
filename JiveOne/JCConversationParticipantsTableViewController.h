1805a4cc08ab0bcb31269f53d9eada31d74cb58b
Eduardo Gueiros <egueiros@jive.com>
2014-05-27 17:14:08 -0600

    [US3455] Added Participants view controller

---
+++ JiveOne/JCConversationParticipantsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JCConversationParticipantsTableViewController.h
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 5/27/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCConversationParticipantsTableViewController : UITableViewController
+
+@end
