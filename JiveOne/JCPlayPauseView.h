c5c2d11f42c1b4b19559b7846cf14bc32439f25b
Doug <doug@jive.com>
2014-05-28 14:34:47 -0600

    TA9079 - refactor playPause

---
+++ JiveOne/JCPlayPauseView.h
@@ -0,0 +1,13 @@
+//
+//  JCPlayPauseView.h
+//  JiveOne
+//
+//  Created by Doug on 5/28/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCPlayPauseView : UIView
+
+@end
