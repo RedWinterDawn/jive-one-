91a1645aaab61c4068a88ed8692bcfedfaab7151
Eduardo Gueiros <egueiros@jive.com>
2014-06-12 20:06:45 -0600

    [US3755] Added live API tests to project

---
+++ JiveOneTests/JCRestClientLiveTests.m
@@ -0,0 +1,34 @@
+//
+//  JCRestClientLiveTests.m
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 6/12/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JCRestClientLiveTests : XCTestCase
+
+@end
+
+@implementation JCRestClientLiveTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
