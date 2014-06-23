745fa0cb54f5767a911f8ac07b25e589d6d0e62b
Daniel George <danielsgeorge@gmail.com>
2014-06-23 10:36:25 -0600

    added additions extension class. added logintestcase example class

---
+++ JiveOneTests/kif/JCLoginTestCase.m
@@ -0,0 +1,36 @@
+//
+//  JCLoginTestCase.m
+//  JiveOne
+//
+//  Created by Daniel George on 6/23/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <KIF/KIF.h>
+#import "KIFUITestActor+Additions.h"
+
+@interface JCLoginTestCase : KIFTestCase
+
+@end
+
+@implementation JCLoginTestCase
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
---
+++ JiveOneTests/kif/KIFUITestActor+Additions.h
@@ -0,0 +1,14 @@
+//
+//  KIFUITestActor1.h
+//  JiveOne
+//
+//  Created by Daniel George on 6/23/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import "KIFUITestActor.h"
+#import <KIF/KIF.h>
+
+@interface KIFUITestActor (Additions)
+
+@end
