613cfe2f0a37de0f08a34f6296ae2ef460c37501
Eduardo Gueiros <egueiros@jive.com>
2014-06-25 12:20:13 -0600

    [NO STORY] Created ContactsRestClient instead of altering the existing rest client.

---
+++ JiveOne/JCContactsClient.h
@@ -0,0 +1,21 @@
+//
+//  JCContactsClient.h
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 6/24/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JCContactsClient : NSObject
+
+@property (nonatomic, strong) AFHTTPRequestOperationManager *manager;
+
++ (instancetype)sharedClient;
+
+- (void)RetrieveMyInformation:(void (^)(BOOL suceeded, id responseObject, AFHTTPRequestOperation *operation, NSError *error))completed;
+- (void)RetrieveContacts:(void (^)(BOOL suceeded, id responseObject, AFHTTPRequestOperation *operation, NSError *error))completed;
+
+- (void)SubscribeToSocketEvents:(NSString *)subscriptionURL dataDictionary:(NSDictionary *)dataDictionary;
+@end
---
+++ JiveOne/JCContactsClient.m
@@ -0,0 +1,138 @@
+//
+//  JCContactsClient.m
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 6/24/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import "JCContactsClient.h"
+#import "PBX+Custom.h"
+#import "Lines+Custom.h"
+#import "Membership+Custom.h"
+
+@implementation JCContactsClient
+{
+    KeychainItemWrapper *keyChainWrapper;
+    NSManagedObjectContext *localContext;
+}
+
+#pragma mark - class methods
+
++ (JCContactsClient*)sharedClient {
+    static JCContactsClient *_sharedClient = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedClient = [[super alloc] init];
+        [_sharedClient initialize];
+    });
+    return _sharedClient;
+}
+
+-(void)initialize
+{
+    
+    //TODO:implement AFCompoundSerializer This is useful for supporting multiple potential types and structures of server responses with a single serializer. @dleonard00 3/14/14
+    NSURL *baseURL = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@", kOsgiBaseURL, kOsgiURNScheme]];
+    _manager = [[AFHTTPRequestOperationManager alloc] initWithBaseURL:baseURL];
+    _manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    //_manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    
+    keyChainWrapper = [[KeychainItemWrapper alloc] initWithIdentifier:kJiveAuthStore accessGroup:nil];
+    localContext  = [NSManagedObjectContext MR_contextForCurrentThread];
+    
+    NSLog(@"About to go into debug mode for server certificate");
+#if DEBUG
+    NSLog(@"Debug mode active");
+    _manager.securityPolicy = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModeNone];
+    _manager.securityPolicy.allowInvalidCertificates = YES;
+#endif
+}
+
+
+- (void)setRequestAuthHeader
+{
+    
+    KeychainItemWrapper* _keychainWrapper = [[KeychainItemWrapper alloc] initWithIdentifier:kJiveAuthStore accessGroup:nil];
+    NSString *token = [_keychainWrapper objectForKey:(__bridge id)(kSecAttrAccount)];
+    
+    [self clearCookies];
+    
+    if (!token) {
+        token = (NSString*)[[NSUserDefaults standardUserDefaults] objectForKey:@"authToken"];
+    }
+    
+    _manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    [_manager.requestSerializer clearAuthorizationHeader];
+    [_manager.requestSerializer setValue:token forHTTPHeaderField:@"Authorization"];
+    [_manager.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+}
+
+- (void)clearCookies {
+    
+    //This will delete ALL cookies.
+    NSHTTPCookieStorage *cookieJar = [NSHTTPCookieStorage sharedHTTPCookieStorage];
+    
+    for (NSHTTPCookie *cookie in [cookieJar cookies]) {
+        [[NSHTTPCookieStorage sharedHTTPCookieStorage] deleteCookie:cookie];
+    }
+    
+}
+
+#pragma mark - class methods
+
+- (void)RetrieveMyInformation:(void (^)(BOOL suceeded, id responseObject, AFHTTPRequestOperation *operation, NSError *error))completed
+{
+    [self setRequestAuthHeader];
+    
+//    NSString * url = [NSString stringWithFormat:@"%@%@", [_manager baseURL], kOsgiMyEntityRoute];//TODO: not attaching baseURL to route constant
+    NSString *username = [[NSUserDefaults standardUserDefaults] objectForKey:@"username"];
+    NSString *url = [NSString stringWithFormat:@"https://api.jive.com/contacts/jiveuser/info/jiveid/%@", username];
+    
+    NSLog(@"%@", url);
+    
+    [_manager GET:url parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        
+        [Membership addMemberships:responseObject completed:^(BOOL suceeded) {
+            completed(YES, responseObject, operation, nil);
+        }];
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        completed(NO, nil, operation, error);
+    }];
+}
+
+- (void)RetrieveContacts:(void (^)(BOOL suceeded, id responseObject, AFHTTPRequestOperation *operation, NSError *error))completed
+{
+    [self setRequestAuthHeader];
+    
+    Lines *line = [Lines MR_findFirst];
+    NSString *url = [NSString stringWithFormat:@"https://api.jive.com/contacts/%@/line/id/%@", line.pbxId, line.lineId];
+    
+    [_manager GET:url parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSArray *contactArray = (NSArray *)responseObject;
+        if (contactArray) {
+            [Lines addLines:contactArray pbxId:line.pbxId completed:^(BOOL succeeded) {
+                completed(succeeded, responseObject, operation, nil);
+            }];
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        completed(NO, nil, operation, error);
+    }];
+    
+}
+
+- (void)SubscribeToSocketEvents:(NSString *)subscriptionURL dataDictionary:(NSDictionary *)dataDictionary
+{
+    [self setRequestAuthHeader];
+    
+    [_manager POST:subscriptionURL parameters:dataDictionary success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"Success");
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"%@", error);
+        // boo!
+    }];
+}
+
+
+@end
