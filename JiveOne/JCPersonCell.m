b6ae3299cb7b15f0e33deaf8dee6ce8ca12cc230
Douglas <doug@jive.com>
2014-07-09 16:58:36 -0600

    US4528 - clean up and fix what broke in my hasty merge

--- JiveOne/JCPersonCell.m
+++ JiveOne/JCPersonCell.m
@@ -25,7 +25,7 @@
     if (self) {
         NSArray *bundleArray = [[NSBundle mainBundle] loadNibNamed:@"JCPersonCell" owner:self options:nil];
         self = bundleArray[0];
-
+        
     }
     return self;
 }
@@ -44,77 +44,39 @@
     [self bringSubviewToFront:self.favoriteBut];
 }
 
-- (void)configureFavoriteStatus
-{
-    UIColor *StarColor;
-    UIColor *StarColor1;
-    
-    //    if ([self.line.isFavorite  isEqual: @1]) {
-    StarColor = [UIColor colorWithRed:255.0/255.0 green:212.0/255.0 blue:0.0/255.0 alpha:1.0];
-    NSMutableAttributedString *attributedStarSelectedState = [[NSMutableAttributedString alloc]initWithString:@"★" attributes:@{NSForegroundColorAttributeName : StarColor}];
-    [self.favoriteBut setAttributedTitle:attributedStarSelectedState forState:UIControlStateSelected];
-    //    }else
-    //    {
-    StarColor1 = [UIColor colorWithRed:208.0/255.0 green:208.0/255.0 blue:208.0/255.0 alpha:1.0];
-    //    }
-    
-    NSMutableAttributedString *attributedStarSelectedState1 = [[NSMutableAttributedString alloc]initWithString:@"★" attributes:@{NSForegroundColorAttributeName : StarColor1}];
-    [self.favoriteBut setAttributedTitle:attributedStarSelectedState1 forState:UIControlStateNormal];
-    if ([self.line.isFavorite  isEqual: @1]) {
-
-        [self.favoriteBut setSelected:YES];
-    }else{
-        [self.favoriteBut setSelected:NO];
-    }
-//    UIColor *StarColor;
-//    
-//    if (self.person.isFavorite || self.line.isFavorite) {
-//        StarColor = [UIColor colorWithRed:255.0/255.0 green:212.0/255.0 blue:0.0/255.0 alpha:1.0];
-//    }else
-//    {
-//        StarColor = [UIColor colorWithRed:208.0/255.0 green:208.0/255.0 blue:208.0/255.0 alpha:1.0];
-//    }
-//    
-//    NSMutableAttributedString *attributedStarSelectedState = [[NSMutableAttributedString alloc]initWithString:@"★" attributes:@{NSForegroundColorAttributeName : StarColor, NSFontAttributeName:[UIFont systemFontOfSize:22.0f]}];
-//    
-//    [self.favoriteBut setAttributedTitle:attributedStarSelectedState forState:UIControlStateNormal];
-}
 
 - (void)setPerson:(PersonEntities *)person
 {
-    //[self removeObservers];
-    if ([person isKindOfClass:[PersonEntities class]]) {
-        _person = person;
-                
-        [self.personNameLabel setNumberOfLines:0];
-        self.personNameLabel.text = person.firstLastName;
-        [self.personNameLabel sizeToFit];
-        self.personDetailLabel.text = person.email;
-        self.personPresenceView.presenceType = (JCPresenceType)[_person.entityPresence.interactions[@"chat"][@"code"] integerValue];
-        
-        // Set person's image based on whether they actually have one or not
-        [self setPersonImage];
-        //[self.personPicture setImageWithURL:[NSURL URLWithString:person.picture] placeholderImage:[UIImage imageNamed:@"avatar.png"]];
-        
-        //[person addObserver:self forKeyPath:kPresenceKeyPathForClientEntity options:NSKeyValueObservingOptionNew context:NULL];
-    }
-    else if ([person isKindOfClass:[Lines class]])
-    {
-        Lines *line = (Lines *)person;
-        _line = line;
-        [self.personNameLabel setNumberOfLines:0];
-        self.personNameLabel.text = line.displayName;
-        [self.personNameLabel sizeToFit];
-        //[self configureFavoriteStatus];
-        self.personDetailLabel.text = line.externsionNumber;
-        self.personPresenceView.presenceType = (JCPresenceType) [line.state integerValue]; //JCPresenceTypeAvailable;// (JCPresenceType)[_person.entityPresence.interactions[@"chat"][@"code"] integerValue];
-        
-        // Set person's image based on whether they actually have one or not
-        //[self setPersonImage];
-        //[self.personPicture setImageWithURL:[NSURL URLWithString:person.picture] placeholderImage:[UIImage imageNamed:@"avatar.png"]];
-        
-		[self updateFavoriteIcon:self];
-        [line addObserver:self forKeyPath:kPresenceKeyPathForLineEntity options:NSKeyValueObservingOptionNew context:NULL];
+    if ([person isKindOfClass:[Lines class]])
+        {
+            Lines *line = (Lines *)person;
+            _line = line;
+            [self.personNameLabel setNumberOfLines:0];
+            self.personNameLabel.text = line.displayName;
+            [self.personNameLabel sizeToFit];
+            [self configureFavoriteStatus];
+            self.personDetailLabel.text = line.externsionNumber;
+            self.personPresenceView.presenceType = (JCPresenceType) [line.state integerValue];
+            [line addObserver:self forKeyPath:kPresenceKeyPathForLineEntity options:NSKeyValueObservingOptionNew context:NULL];
+        }
+    
+}
+
+- (void)configureFavoriteStatus
+{
+    //selected star should be yellow unselected star should be gray
+    UIColor *selectedStarColor = [UIColor colorWithRed:255.0/255.0 green:212.0/255.0 blue:0.0/255.0 alpha:1.0];
+    NSMutableAttributedString *selectedAttributedStarSelectedState = [[NSMutableAttributedString alloc]initWithString:@"★" attributes:@{NSForegroundColorAttributeName : selectedStarColor}];
+    UIColor *unselectedStarColor = [UIColor colorWithRed:208.0/255.0 green:208.0/255.0 blue:208.0/255.0 alpha:1.0];
+    
+    NSMutableAttributedString *unselectedAttributedStarSelectedState = [[NSMutableAttributedString alloc]initWithString:@"★" attributes:@{NSForegroundColorAttributeName : unselectedStarColor}];
+    [self.favoriteBut setAttributedTitle:selectedAttributedStarSelectedState forState:UIControlStateSelected];
+    [self.favoriteBut setAttributedTitle:unselectedAttributedStarSelectedState forState:UIControlStateNormal];
+    
+    if ([self.line.isFavorite  isEqual: @1]) {
+        [self.favoriteBut setSelected:YES];
+    }else{
+        [self.favoriteBut setSelected:NO];
     }
 }
 
@@ -138,7 +100,6 @@
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
 {
     if ([keyPath isEqualToString:kPresenceKeyPathForLineEntity]) {
-        //PersonEntities *person = (PersonEntities *)object;
         Lines *line = (Lines *)object;
         self.personPresenceView.presenceType = (JCPresenceType)[line.state integerValue];
     }
@@ -161,93 +122,49 @@
     return 0;
 }
 
-- (void)setPersonImage
-{
-    
-    
-//    NSURL *imageUrl = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@", kOsgiBaseURL, self.person.picture]];
-//    NSRange range = [[imageUrl description] rangeOfString:@"avatar"];
-//    if (range.location == NSNotFound) {
-//        [self.personPicture setImageWithURL:imageUrl placeholderImage:[UIImage imageNamed:@"avatar.png"]];
-//    }
-//    else {
-//        
-//        NSString *firstInitial = @"";
-//        NSString *secondInitial = @"";
-//        
-//        if (self.person.firstName && [self.person.firstName length] > 0) {
-//            firstInitial = [self.person.firstName substringToIndex:1];
-//        }
-//        else {
-//            [self.personPicture setImageWithURL:imageUrl placeholderImage:[UIImage imageNamed:@"avatar.png"]];
-//            return;
-//        }
-//        
-//        if (self.person.lastName && [self.person.lastName length] > 0) {
-//            secondInitial = [self.person.lastName substringToIndex:1];
-//        }
-//        else {
-//            [self.personPicture setImageWithURL:imageUrl placeholderImage:[UIImage imageNamed:@"avatar.png"]];
-//            return;
-//        }
-//        
-//        NSString *key = [NSString stringWithFormat:@"%@%@", firstInitial, secondInitial];
-//        
-//        UIImage *initialsImage = [[JCPersonCell chachedInitialsImages] objectForKey:key];
-//        if (!initialsImage) {
-//            UIView * groupCount = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 28, 28)];
-//            groupCount.backgroundColor = [UIColor colorWithRed:0.847 green:0.871 blue:0.882 alpha:1] /*#d8dee1*/;
-//            
-//            UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 20)];
-//            label.textColor = [UIColor whiteColor];           
-//            
-//            label.text = [NSString stringWithFormat:@"%@%@", firstInitial, secondInitial];
-//            [label sizeToFit];
-//            label.font = [UIFont boldSystemFontOfSize:12.0f];
-//            label.center = groupCount.center;
-//            label.bounds = CGRectInset(label.frame, 2.5f, 0);
-//            [groupCount addSubview:label];
-//            
-//            initialsImage = [Common imageFromView:groupCount];
-//            [[JCPersonCell chachedInitialsImages] setObject:initialsImage forKey:key];
-//        }
-//        else {
-////            NSLog(@"Cache hit for key: %@", key);
-//        }
-//        
-//        [self.personPicture setImage:initialsImage];
-//    }
-}
 
 /**
-* Image Cache
-*
-* As images are created, the are added to the image cache. We have a static
-* mutable array that is shared by all instaces of the initialsImage. We use the
-* dispatch once to ensure that it is only ever instanced once.
-*/
+ * Image Cache
+ *
+ * As images are created, the are added to the image cache. We have a static
+ * mutable array that is shared by all instaces of the initialsImage. We use the
+ * dispatch once to ensure that it is only ever instanced once.
+ */
 + (NSMutableDictionary *)chachedInitialsImages {
-  static NSMutableDictionary *cachedPresenceImages = nil;
-  static dispatch_once_t oncePredicate;
-  dispatch_once(&oncePredicate, ^{
-      cachedPresenceImages = [NSMutableDictionary new];
-  });
-  return cachedPresenceImages;
+    static NSMutableDictionary *cachedPresenceImages = nil;
+    static dispatch_once_t oncePredicate;
+    dispatch_once(&oncePredicate, ^{
+        cachedPresenceImages = [NSMutableDictionary new];
+    });
+    return cachedPresenceImages;
 }
 
 - (IBAction)toggleFavoriteStatus:(id)sender {
     
 	_managedContext = [self managedContext];
-	
-    self.line.isFavorite = [NSNumber numberWithBool:![self.line.isFavorite boolValue]];
-	
+		
+    if ([self.line.isFavorite  isEqual: @1]) {
+        self.line.isFavorite = @0;
+        [self.favoriteBut setSelected:NO];
+    }else{
+        self.line.isFavorite = @1;
+        [self.favoriteBut setSelected:YES];
+    }
+    NSLog(@"self.line.isFavorite:%@",self.line.isFavorite);
+    
+    [self groupRelationshipStuff];
+    [_managedContext MR_saveToPersistentStoreAndWait];
+}
+
+- (void)groupRelationshipStuff
+{
 	ContactGroup *group = [ContactGroup MR_findFirstByAttribute:@"groupName" withValue:@"Favorites"];
 	if (!group) {
 		group = [ContactGroup MR_createInContext:_managedContext];
 		group.groupId = [[NSUUID UUID] UUIDString];
 		group.groupName = @"Favorites";
 	}
-	
+    
 	// create relationship
 	NSPredicate *pred = [NSPredicate predicateWithFormat:@"(groupId == %@) AND (lineId == %@)", group.groupId, self.line.jrn];
 	LineGroup *lg = [LineGroup MR_findFirstWithPredicate:pred];
@@ -259,32 +176,67 @@
 	else  if (lg && ![self.line.isFavorite boolValue])
 	{
 		[lg MR_deleteEntity];
-	}	
-
-    [_managedContext MR_saveToPersistentStoreAndWait];
-    
-    [self updateFavoriteIcon:self];
+	}
 }
-
-- (void) updateFavoriteIcon:(JCPersonCell *)cell
-{
-//    UIColor *StarColor;
-//    UIColor *StarColor1;
-//
-////    if ([self.line.isFavorite  isEqual: @1]) {
-//        StarColor = [UIColor colorWithRed:255.0/255.0 green:212.0/255.0 blue:0.0/255.0 alpha:1.0];
-//        NSMutableAttributedString *attributedStarSelectedState = [[NSMutableAttributedString alloc]initWithString:@"★" attributes:@{NSForegroundColorAttributeName : StarColor}];
-//        [self.favoriteBut setAttributedTitle:attributedStarSelectedState forState:UIControlStateNormal];
-////    }else
-////    {
-//        StarColor1 = [UIColor colorWithRed:208.0/255.0 green:208.0/255.0 blue:208.0/255.0 alpha:1.0];
-////    }
-//    
-//    NSMutableAttributedString *attributedStarSelectedState1 = [[NSMutableAttributedString alloc]initWithString:@"★" attributes:@{NSForegroundColorAttributeName : StarColor1}];
-//    [self.favoriteBut setAttributedTitle:attributedStarSelectedState1 forState:UIControlStateSelected];
-    
-//    self.favoriteBut.hidden = YES;
-}
-
 
 @end
+
+
+
+//- (void)setPersonImage
+//{
+
+    
+    //    NSURL *imageUrl = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@", kOsgiBaseURL, self.person.picture]];
+    //    NSRange range = [[imageUrl description] rangeOfString:@"avatar"];
+    //    if (range.location == NSNotFound) {
+    //        [self.personPicture setImageWithURL:imageUrl placeholderImage:[UIImage imageNamed:@"avatar.png"]];
+    //    }
+    //    else {
+    //
+    //        NSString *firstInitial = @"";
+    //        NSString *secondInitial = @"";
+    //
+    //        if (self.person.firstName && [self.person.firstName length] > 0) {
+    //            firstInitial = [self.person.firstName substringToIndex:1];
+    //        }
+    //        else {
+    //            [self.personPicture setImageWithURL:imageUrl placeholderImage:[UIImage imageNamed:@"avatar.png"]];
+    //            return;
+    //        }
+    //
+    //        if (self.person.lastName && [self.person.lastName length] > 0) {
+    //            secondInitial = [self.person.lastName substringToIndex:1];
+    //        }
+    //        else {
+    //            [self.personPicture setImageWithURL:imageUrl placeholderImage:[UIImage imageNamed:@"avatar.png"]];
+    //            return;
+    //        }
+    //
+    //        NSString *key = [NSString stringWithFormat:@"%@%@", firstInitial, secondInitial];
+    //
+    //        UIImage *initialsImage = [[JCPersonCell chachedInitialsImages] objectForKey:key];
+    //        if (!initialsImage) {
+    //            UIView * groupCount = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 28, 28)];
+    //            groupCount.backgroundColor = [UIColor colorWithRed:0.847 green:0.871 blue:0.882 alpha:1] /*#d8dee1*/;
+    //
+    //            UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 20)];
+    //            label.textColor = [UIColor whiteColor];
+    //
+    //            label.text = [NSString stringWithFormat:@"%@%@", firstInitial, secondInitial];
+    //            [label sizeToFit];
+    //            label.font = [UIFont boldSystemFontOfSize:12.0f];
+    //            label.center = groupCount.center;
+    //            label.bounds = CGRectInset(label.frame, 2.5f, 0);
+    //            [groupCount addSubview:label];
+    //
+    //            initialsImage = [Common imageFromView:groupCount];
+    //            [[JCPersonCell chachedInitialsImages] setObject:initialsImage forKey:key];
+    //        }
+    //        else {
+    ////            NSLog(@"Cache hit for key: %@", key);
+    //        }
+    //
+    //        [self.personPicture setImage:initialsImage];
+    //    }
+//}
