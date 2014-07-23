6eb662651677def5ac4b1de451a38fb7abfbf847
P Leonard <danielleonard@getjive.com>
2014-07-23 17:44:07 -0600

    Terms of service login screen test

---
+++ JiveOneTests/integration/javascript/iphone/TermsAndConditionsLinkFromLoginPage.js
@@ -0,0 +1,34 @@
+#import "../Common.js"
+test("Terms and Conditions link from login page", function(target, app){
+	logoutIfNeeded(app);
+	//Dismiss the keybaord if preasent
+	target.tap({x:287.50, y:112.00});
+	target.frontMostApp().logElementTree();
+	//Touch Terms and Conditions link at bottom of page
+	target.frontMostApp().mainWindow().buttons()["Terms and Conditions."].tap();
+	delay(4);
+	 //Select the Menu
+	target.frontMostApp().mainWindow().scrollViews()[0].webViews()[0].buttons()[""].tap();
+	assertTrue(target.frontMostApp().mainWindow().scrollViews()[0].webViews()[0].links()["ENTERPRISE"],"ENTERPRISE tab not found");
+	target.frontMostApp().mainWindow().scrollViews()[0].webViews()[0].links()["GOVERNMENT"].tap();
+	delay(3);
+	target.frontMostApp().logElementTree();
+	//Test all the navagation buttons on the bottom of screen
+	target.frontMostApp().toolbar().buttons()["Back"].tap();
+	delay(2);
+	target.frontMostApp().toolbar().buttons()["Forward"].tap();
+	delay(2);
+	target.frontMostApp().toolbar().buttons()["Back"].tap();
+	delay(2);
+	target.frontMostApp().toolbar().buttons()["Refresh"].tap();
+	target.frontMostApp().toolbar().buttons()["Share"].tap();
+	delay(2);
+	target.frontMostApp().actionSheet().cancelButton().tap();
+	delay(2);
+	target.frontMostApp().mainWindow().scrollViews()[0].webViews()[0].tapWithOptions({tapOffset:{x:0.12, y:0.01}});
+	target.frontMostApp().navigationBar().leftButton().tap();
+
+	//We should now be in the login screen
+
+
+});
