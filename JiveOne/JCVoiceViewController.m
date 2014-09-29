408b5372d1b044add3ecfee4a6fda6443fcf3a37
Robert Barclay <rbarclay@jive.com>
2014-09-29 12:50:52 -0600

    Built new wrapper class for Voicemail, moving the Voicemail table view controller to be an embedded view controller in a wrapper Voicmail View Controller, and added appropriate logic to determine if v4 vs v5 functionality should be shown. Modified the iPhone Storyboard to incorporate new wrapper logic. Defined initial starting point for the non-visual voicemail UI, with linked IBAction setup for a button.

---
+++ JiveOne/JCVoiceViewController.m
@@ -0,0 +1,57 @@
+//
+//  JCVoiceViewController.m
+//  JiveOne
+//
+//  This is a wrapper view controller that encapsulates v4 and v5 voicemail functionality. v4 does not have visual
+//  voicemail, so we need to present a different UI when thier PBX is a v4 account. When the PBX is a v5 Account, normal
+//  workflow is followed, loading the visual table view controller.
+//
+//  Created by Robert Barclay on 9/29/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import "JCVoiceViewController.h"
+
+#import "JCVoiceTableViewController.h"
+#import "PBX+Custom.h"
+
+NSString *const kJCVoiceNonVisualViewControllerIdentifier = @"VoiceNonVisualViewController";
+
+@interface JCVoiceViewController ()
+{
+    JCVoiceTableViewController *_voiceTableViewController;
+}
+
+@end
+
+@implementation JCVoiceViewController
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    PBX *pbx = [PBX fetchFirstPBX];
+    if (!pbx.v5)
+    {
+        [_voiceTableViewController.view removeFromSuperview];
+        [_voiceTableViewController removeFromParentViewController];
+        
+        @try {
+            UIViewController *nonVisualViewController = [self.storyboard instantiateViewControllerWithIdentifier:kJCVoiceNonVisualViewControllerIdentifier];
+            [self addChildViewController:nonVisualViewController];
+            [self.containerView addSubview:nonVisualViewController.view];
+        }
+        @catch (NSException *exception) {
+            NSLog(@"Non Visual Voicmail View Controller was unable to to loaded from the storyboard, was expecting identifier: %@", kJCVoiceNonVisualViewControllerIdentifier);
+        }
+    }
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    
+    UIViewController *viewController = segue.destinationViewController;
+    if ([viewController isKindOfClass:[JCVoiceTableViewController class]])
+        _voiceTableViewController = (JCVoiceTableViewController *)viewController;
+}
+
+@end
