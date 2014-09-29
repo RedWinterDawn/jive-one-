4f7daddcb6a0b99ca1bb4d1d9d581ba1380ac4be
Daniel George <danielsgeorge@gmail.com>
2014-09-29 14:43:19 -0600

    added dialer ViewController

---
+++ JiveOne/JCDialerViewController.h
@@ -0,0 +1,13 @@
+//
+//  JCDialerViewController.h
+//  JiveOne
+//
+//  Created by Daniel George on 9/29/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCDialerViewController : UIViewController
+
+@end
