4ddbafa432ad085de73febb8c190590aecffb535
Pete <plen@jive.com>
2014-09-29 15:30:04 -0600

    Adding the dialer and navigation img assets

---
+++ JiveOne/JCDialerViewController.h
@@ -0,0 +1,13 @@
+//
+//  JCDialerViewController.h
+//  JiveOne
+//
+//  Created by P Leonard on 9/29/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCDialerViewController : UIViewController
+
+@end
