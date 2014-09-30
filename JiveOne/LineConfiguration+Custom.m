12169b093aee2d7317d2c1efc1b934c405edd6fe
Eduardo Gueiros <egueiros@jive.com>
2014-09-30 03:02:50 -0600

    added core data schema which will be used to persist configuration data.

---
+++ JiveOne/LineConfiguration+Custom.m
@@ -0,0 +1,13 @@
+//
+//  LineConfiguration+Custom.m
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 9/30/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import "LineConfiguration+Custom.h"
+
+@implementation LineConfiguration (Custom)
+
+@end
