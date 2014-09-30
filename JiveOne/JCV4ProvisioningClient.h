7cda62386377eae14ae4db1bf05226d65a0771bd
Eduardo Gueiros <egueiros@jive.com>
2014-09-30 02:36:49 -0600

    added clients to handle the request and retrieval of provisioning profile

---
+++ JiveOne/JCV4ProvisioningClient.h
@@ -0,0 +1,13 @@
+//
+//  JCV4ProvisioningClient.h
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 9/30/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JCV4ProvisioningClient : NSObject
+
+@end
---
+++ JiveOne/JCV4ProvisioningClient.m
@@ -0,0 +1,13 @@
+//
+//  JCV4ProvisioningClient.m
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 9/30/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import "JCV4ProvisioningClient.h"
+
+@implementation JCV4ProvisioningClient
+
+@end
