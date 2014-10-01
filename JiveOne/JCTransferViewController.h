acfbab7128f3bca190f252b22b06d3d2ec93bf32
Robert Barclay <rbarclay@jive.com>
2014-10-01 10:11:48 -0600

    Wired up the Transfer controller to the storyboard

---
+++ JiveOne/JCTransferViewController.h
@@ -0,0 +1,13 @@
+//
+//  JCTransferViewController.h
+//  JiveOne
+//
+//  Created by Robert Barclay on 10/1/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import "JCDialerViewController.h"
+
+@interface JCTransferViewController : JCDialerViewController
+
+@end
