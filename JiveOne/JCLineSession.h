1525e4885349e0c7ba4c2ce19b1503db24f3fa82
Eduardo Gueiros <egueiros@jive.com>
2014-10-02 11:28:17 -0600

    added line session class

---
+++ JiveOne/JCLineSession.h
@@ -0,0 +1,13 @@
+//
+//  JCLineSession.h
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 10/1/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JCLineSession : NSObject
+
+@end
---
+++ JiveOne/JCLineSession.m
@@ -0,0 +1,13 @@
+//
+//  JCLineSession.m
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 10/1/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import "JCLineSession.h"
+
+@implementation JCLineSession
+
+@end
