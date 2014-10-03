721d20463202894fe74e909d14bfe57aba788c85
Eduardo Gueiros <egueiros@jive.com>
2014-10-03 08:14:23 -0600

    created v5Client class where all API calls are made

---
+++ JiveOne/JCV5ApiClient.h
@@ -0,0 +1,13 @@
+//
+//  JCV5ApiClient.h
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 10/2/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JCV5ApiClient : NSObject
+
+@end
---
+++ JiveOne/JCV5ApiClient.m
@@ -0,0 +1,13 @@
+//
+//  JCV5ApiClient.m
+//  JiveOne
+//
+//  Created by Eduardo Gueiros on 10/2/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import "JCV5ApiClient.h"
+
+@implementation JCV5ApiClient
+
+@end
