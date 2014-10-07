9bd0fc47b2b2abd95b7c023dc79eee06901d6c6b
Pete <plen@jive.com>
2014-10-07 10:41:58 -0600

    Fixing the subview dialer to make it viewable and touchable

--- JiveOne/JCKeyboardViewController.m
+++ JiveOne/JCKeyboardViewController.m
@@ -17,10 +17,12 @@
         UIButton *button = (UIButton *)sender;
         NSString *typedChar = [self characterFromNumPadTag:button.tag];
         
+        self.outputLabel.text =  [NSString stringWithFormat:@"%@%@", self.outputLabel.text, typedChar];
+        
         if (_delegate && [_delegate respondsToSelector:@selector(keyboardViewController:didTypeNumber:)])
             [_delegate keyboardViewController:self didTypeNumber:typedChar];
         
-        self.outputLabel.text =  [NSString stringWithFormat:@"%@%@", self.outputLabel.text, typedChar];
+        
     }
 }
 
