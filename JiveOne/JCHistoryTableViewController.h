eaab3a2743076735d85bc444170f62e4fd191617
Pete <plen@jive.com>
2014-10-13 15:28:37 -0600

    added Call History view and call imgs

---
+++ JiveOne/JCHistoryTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  JCHistoryTableViewController.h
+//  JiveOne
+//
+//  Created by P Leonard on 10/13/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "JCHistoryCell.h"
+
+@interface JCHistoryTableViewController : UITableViewController
+@property (weak, nonatomic) IBOutlet UISegmentedControl *historySegmentedControl;
+
+@end
---
+++ JiveOne/JCHistoryTableViewController.m
@@ -0,0 +1,65 @@
+//
+//  JCHistoryTableViewController.m
+//  JiveOne
+//
+//  Created by P Leonard on 10/13/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import "JCHistoryTableViewController.h"
+
+
+@interface JCHistoryTableViewController ()
+
+@end
+
+@implementation JCHistoryTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    /*[self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleSingleLine];
+    [self.tableView setSeparatorInset:UIEdgeInsetsZero];
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"main-logo.png"]];
+    imageView.contentMode = UIViewContentModeCenter;
+    self.tableView.backgroundView = imageView;*/
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+
+@end
