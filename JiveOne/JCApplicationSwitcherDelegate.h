36c8ec0778c02607c3a725007769da6bb45ae8a8
Robert Barclay <rbarclay@jive.com>
2014-10-27 22:03:05 -0600

    Added the Application Switcher classes to the project.

---
+++ JiveOne/JCApplicationSwitcherDelegate.h
@@ -0,0 +1,14 @@
+//
+//  JCApplicationSwitcherDataSource.h
+//  JCApplicationSwitcher
+//
+//  Created by Robert Barclay on 10/17/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "JCApplicationSwitcherViewController.h"
+
+@interface JCApplicationSwitcherDelegate : NSObject <JCApplicationSwitcherDelegate>
+
+@end
---
+++ JiveOne/JCApplicationSwitcherDelegate.m
@@ -0,0 +1,40 @@
+//
+//  JCApplicationSwitcherDataSource.m
+//  JCApplicationSwitcher
+//
+//  Created by Robert Barclay on 10/17/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import "JCApplicationSwitcherDelegate.h"
+#import "JCMenuBarButtonItem.h"
+
+@implementation JCApplicationSwitcherDelegate
+
+-(NSArray *)applicationSwitcherController:(JCApplicationSwitcherViewController *)controller willLoadViewControllers:(NSArray *)viewControllers
+{
+    return viewControllers;
+}
+
+-(UIBarButtonItem *)applicationSwitcherController:(JCApplicationSwitcherViewController *)controller identifier:(NSString *)identifier;
+{
+    return [[JCMenuBarButtonItem alloc] initWithTarget:controller action:@selector(showMenu:)];
+}
+
+-(UIViewController *)applicationSwitcherLastSelectedViewController:(JCApplicationSwitcherViewController *)controller
+{
+    return nil;
+}
+
+-(void)applicationSwitcherController:(JCApplicationSwitcherViewController *)controller willSelectViewController:(UIViewController *)viewController
+{
+    
+}
+
+-(void)applicationSwitcherController:(JCApplicationSwitcherViewController *)controller didSelectViewController:(UIViewController *)viewController
+{
+    
+}
+
+
+@end
