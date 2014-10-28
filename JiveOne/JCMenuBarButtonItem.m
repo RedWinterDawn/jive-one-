cd146011b4c5e1acd2c91948c0c39cd181e6029d
Robert Barclay <rbarclay@jive.com>
2014-10-28 15:48:04 -0600

    Adjusted the drawing of the App Switcher menu button.

--- JiveOne/JCMenuBarButtonItem.m
+++ JiveOne/JCMenuBarButtonItem.m
@@ -32,6 +32,8 @@
         
         UIGraphicsBeginImageContextWithOptions( CGSizeMake(26, 26), NO, 0 );
         
+        CGFloat width = 2;
+        
         //// Color Declarations
         UIColor *fillColor = [UIColor whiteColor];
         
@@ -39,17 +41,17 @@
         CGRect frame = CGRectMake(0, 0, 26, 26);
         
         //// Bottom Bar Drawing
-        UIBezierPath *bottomBarPath = [UIBezierPath bezierPathWithRect: CGRectMake(CGRectGetMinX(frame) + floor((CGRectGetWidth(frame) - 16) * 0.50000 + 0.5), CGRectGetMinY(frame) + floor((CGRectGetHeight(frame) - 1) * 0.72000 + 0.5), 16, 1)];
+        UIBezierPath *bottomBarPath = [UIBezierPath bezierPathWithRect: CGRectMake(CGRectGetMinX(frame) + floor((CGRectGetWidth(frame) - 16) * 0.50000 + 0.5), CGRectGetMinY(frame) + floor((CGRectGetHeight(frame) - 1) * 0.68000 + 0.5), 16, width)];
         [fillColor setFill];
         [bottomBarPath fill];
         
         //// Middle Bar Drawing
-        UIBezierPath *middleBarPath = [UIBezierPath bezierPathWithRect: CGRectMake(CGRectGetMinX(frame) + floor((CGRectGetWidth(frame) - 16) * 0.50000 + 0.5), CGRectGetMinY(frame) + floor((CGRectGetHeight(frame) - 1) * 0.48000 + 0.5), 16, 1)];
+        UIBezierPath *middleBarPath = [UIBezierPath bezierPathWithRect: CGRectMake(CGRectGetMinX(frame) + floor((CGRectGetWidth(frame) - 16) * 0.50000 + 0.5), CGRectGetMinY(frame) + floor((CGRectGetHeight(frame) - 1) * 0.48000 + 0.5), 16, width)];
         [fillColor setFill];
         [middleBarPath fill];
         
         //// Top Bar Drawing
-        UIBezierPath *topBarPath = [UIBezierPath bezierPathWithRect: CGRectMake(CGRectGetMinX(frame) + floor((CGRectGetWidth(frame) - 16) * 0.50000 + 0.5), CGRectGetMinY(frame) + floor((CGRectGetHeight(frame) - 1) * 0.24000 + 0.5), 16, 1)];
+        UIBezierPath *topBarPath = [UIBezierPath bezierPathWithRect: CGRectMake(CGRectGetMinX(frame) + floor((CGRectGetWidth(frame) - 16) * 0.50000 + 0.5), CGRectGetMinY(frame) + floor((CGRectGetHeight(frame) - 1) * 0.28000 + 0.5), 16, width)];
         [fillColor setFill];
         [topBarPath fill];
         
