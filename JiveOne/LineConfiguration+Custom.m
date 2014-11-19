531687a94d2a4d357b9168bb49a483561e64f9d1
Robert Barclay <rbarclay@jive.com>
2014-11-19 12:53:41 -0700

    Modified the reading of the LineConfiguration response to handle multiple line assignments. It saves all of them, but the first one is selected when used. It used to only save one, and given the previous logic, the last one, randomly.

--- JiveOne/LineConfiguration+Custom.m
+++ JiveOne/LineConfiguration+Custom.m
@@ -9,45 +9,61 @@
 #import "LineConfiguration+Custom.h"
 #import <XMLDictionary/XMLDictionary.h>
 
+#import "NSDictionary+Validations.h"
+
+NSString *const kLineConfigurationResponseKey = @"_name";
+NSString *const kLineConfigurationResponseValue = @"_value";
+
+NSString *const kLineConfigurationResponseIdentifierKey         = @"display";
+NSString *const kLineConfigurationResponseRegistrationHostKey   = @"domain";
+NSString *const kLineConfigurationResponseOutboundProxyKey      = @"outboundProxy";
+NSString *const kLineConfigurationResponseSipUsernameKey        = @"username";
+NSString *const kLineConfigurationResponseSipPasswordKey        = @"password";
+
 @implementation LineConfiguration (Custom)
+
++(LineConfiguration *)lineConfigurationForIdentifier:(NSString *)identifier context:(NSManagedObjectContext *)context
+{
+    if (!context) {
+        context = [NSManagedObjectContext MR_contextForCurrentThread];
+    }
+    
+    LineConfiguration *lineConfiguration = [LineConfiguration MR_findFirstByAttribute:@"display" withValue:identifier inContext:context];
+    if (!lineConfiguration) {
+        lineConfiguration = [LineConfiguration MR_createInContext:context];
+        lineConfiguration.display = identifier;
+    }
+    
+    return lineConfiguration;
+}
 
 + (void)addConfiguration:(NSDictionary *)config completed:(void (^)(BOOL success))completed
 {
 	[MagicalRecord saveWithBlock:^(NSManagedObjectContext *localContext) {
 		
 		NSArray *dataArray = [config valueForKeyPath:@"branding.settings_data.core_data_list.account_list.account.data"];
-		
-		
-		if (!localContext) {
-			localContext = [NSManagedObjectContext MR_contextForCurrentThread];
-		}
-		
-		LineConfiguration *lineConfig = [LineConfiguration MR_findFirst];
-		if (!lineConfig) {
-			lineConfig = [LineConfiguration MR_createInContext:localContext];
-		}
-		
-		[dataArray enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-			NSDictionary *dic = (NSDictionary*)obj;
-			if ([[dic objectForKey:@"_name"] isEqualToString:@"domain"]) {
-				lineConfig.registrationHost = [dic objectForKey:@"_value"];
-			}
-			else if ([[dic objectForKey:@"_name"] isEqualToString:@"outboundProxy"]) {
-				lineConfig.outboundProxy = [dic objectForKey:@"_value"];
-			}
-			else if ([[dic objectForKey:@"_name"] isEqualToString:@"username"]) {
-				lineConfig.sipUsername = [dic objectForKey:@"_value"];
-			}
-			else if ([[dic objectForKey:@"_name"] isEqualToString:@"password"]) {
-				lineConfig.sipPassword = [dic objectForKey:@"_value"];
-			}
-			else if ([[dic objectForKey:@"_name"] isEqualToString:@"display"]) {
-				lineConfig.display = [dic objectForKey:@"_value"];
-			}
-			
-		}];
-
-			
+        for (id object in dataArray) {
+            if ([object isKindOfClass:[NSArray class]])
+            {
+                // Normalize Data into Key/value pairs.
+                NSMutableDictionary *lineData = [NSMutableDictionary dictionary];
+                [(NSArray *)object enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+                    NSDictionary *lineDataItem = (NSDictionary*)obj;
+                    NSString *key = [lineDataItem stringValueForKey:kLineConfigurationResponseKey];
+                    NSString *value = [lineDataItem stringValueForKey:kLineConfigurationResponseValue];
+                    [lineData setObject:value forKey:key];
+                }];
+                
+                
+                NSString *identifier = [lineData stringValueForKey:kLineConfigurationResponseIdentifierKey];
+                LineConfiguration *lineConfiguration = [LineConfiguration lineConfigurationForIdentifier:identifier context:localContext];
+                
+                lineConfiguration.registrationHost  = [lineData stringValueForKey:kLineConfigurationResponseRegistrationHostKey];
+                lineConfiguration.outboundProxy     = [lineData stringValueForKey:kLineConfigurationResponseOutboundProxyKey];
+                lineConfiguration.sipUsername       = [lineData stringValueForKey:kLineConfigurationResponseSipUsernameKey];
+                lineConfiguration.sipPassword       = [lineData stringValueForKey:kLineConfigurationResponseSipPasswordKey];
+            }
+        }
 		
 	} completion:^(BOOL success, NSError *error) {
 		completed(success);
