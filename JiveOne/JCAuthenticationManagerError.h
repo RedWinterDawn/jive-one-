4c40e53f6647ae571245f743afc0c7b9c2d07c18
Robert Barclay <rbarclay@jive.com>
2014-11-21 23:53:35 -0700

    Created An Authentication Error Class

---
+++ JiveOne/JCAuthenticationManagerError.h
@@ -0,0 +1,13 @@
+//
+//  JCAuthenticationManagerError.h
+//  JiveOne
+//
+//  Created by Robert Barclay on 11/21/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JCAuthenticationManagerError : NSError
+
+@end
---
+++ JiveOne/JCAuthenticationManagerError.m
@@ -0,0 +1,13 @@
+//
+//  JCAuthenticationManagerError.m
+//  JiveOne
+//
+//  Created by Robert Barclay on 11/21/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import "JCAuthenticationManagerError.h"
+
+@implementation JCAuthenticationManagerError
+
+@end
