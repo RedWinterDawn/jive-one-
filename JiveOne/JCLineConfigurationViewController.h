1c368ec57b9b8a517219e7b9b4044cb15d5892f8
Pete <plen@jive.com>
2014-12-01 17:18:01 -0700

    adding line config files

---
+++ JiveOne/JCLineConfigurationViewController.h
@@ -0,0 +1,13 @@
+//
+//  JCLineConfigurationViewController.h
+//  JiveOne
+//
+//  Created by P Leonard on 12/1/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCLineConfigurationViewController : UIViewController
+
+@end
