9923e4736ef85eb8543921c35e11e2101587c27e
Pete <plen@jive.com>
2014-12-12 11:30:04 -0700

    adding contact cells for local contacts

---
+++ JiveOne/JCExternalContactCell.h
@@ -0,0 +1,14 @@
+//
+//  JCExternalContactCell.h
+//  JiveOne
+//
+//  Created by P Leonard on 12/12/14.
+//  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
+//
+
+
+@interface JCExternalContactCell : UITableViewCell
+
+@property (nonatomic, weak) IBOutlet UILabel *externalNameLabel;
+@property (nonatomic, weak) IBOutlet UILabel *externalNumberLebel;
+@end
