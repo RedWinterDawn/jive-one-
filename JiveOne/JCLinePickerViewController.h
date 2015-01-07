d64ef3e97a78605ff0d67438ed66259108d77f0b
Pete <plen@jive.com>
2015-01-07 16:09:24 -0700

    Hooked up outlets

--- JiveOne/JCLinePickerViewController.h
+++ JiveOne/JCLinePickerViewController.h
@@ -5,9 +5,13 @@
 //  Created by Robert Barclay on 12/8/14.
 //  Copyright (c) 2014 Jive Communications, Inc. All rights reserved.
 //
-
+#import "JCVisualEffectsView.h"
 #import "JCPickerViewController.h"
 
+
 @interface JCLinePickerViewController : JCPickerViewController
+@property (weak, nonatomic) IBOutlet JCVisualEffectsView *logingBackgroundView;
+@property (weak, nonatomic) IBOutlet JCVisualEffectsView *pickerViewBackground;
+
 
 @end
