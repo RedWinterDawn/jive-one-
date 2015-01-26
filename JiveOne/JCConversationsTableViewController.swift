57098796936402f7f4ad96ae0a8db98202f26c35
Robert Barclay <rbarclay@jive.com>
2015-01-26 14:14:51 -0700

    Got conversations tableViewControlelr to load from the fetch results controller

--- JiveOne/JCConversationsTableViewController.swift
+++ JiveOne/JCConversationsTableViewController.swift
@@ -18,15 +18,20 @@ class JCConversationsTableViewController: JCFetchedResultsTableViewController {
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: ConversationCellReuseIdentifer)
         self.tableView.dataSource = self
         
-        let fetchRequest:NSFetchRequest = Message.MR_requestAll();
-        fetchRequest.includesSubentities = true;
-        let fetchedResultsController:NSFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext, sectionNameKeyPath: nil, cacheName: nil);
+        let fetchRequest:NSFetchRequest = Message.MR_requestAll()
+        fetchRequest.includesSubentities = true
+        let sortDescriptor:NSSortDescriptor = NSSortDescriptor(key: "date", ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        //fetchRequest.resultType = NSFetchRequestResultType.DictionaryResultType
+        //fetchRequest.propertiesToGroupBy = ["conversationId"]
+        
+        let fetchedResultsController:NSFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
     }
 
     override func tableView(tableView: UITableView!, cellForObject object: NSObjectProtocol!, atIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
         if object.isKindOfClass(Message) {
             let cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier(ConversationCellReuseIdentifer) as UITableViewCell
-            configureCell(cell, withObject: object);
+            configureCell(cell, withObject: object)
             return cell
         }
         return nil
