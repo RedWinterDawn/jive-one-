caa10f505b84d37b2e24058420fe1bda7d31e4d0
Robert Barclay <rbarclay@jive.com>
2015-02-04 11:39:56 -0700

    Migrated the platform type into the UIDevice+Additions category

--- JiveOne/JCSettingsTableViewController.m
+++ JiveOne/JCSettingsTableViewController.m
@@ -13,7 +13,7 @@
 #import "JCTermsAndConditonsViewController.h"
 #import "JCAuthenticationManager.h"
 #import "JCAppSettings.h"
-#import <sys/sysctl.h>
+
 #import "PBX.h"
 
 NSString *const kJCSettingsTableViewControllerFeebackMessage = @"<strong>Please describe any issues you are experiencing :</strong><br><br><br><br><br><br><br><br><br><br><br><br><br><hr><strong>Device Specs</strong><br>Model: %@ <br> On iOS Version: %@ <br> App Version: %@ <br> Country: %@ <br> UUID : %@  <br> PBX : %@  <br> User : %@  <br> Line : %@  <br> ";
@@ -92,80 +92,22 @@ NSString *const kJCSettingsTableViewControllerFeebackMessage = @"<strong>Please 
         [mailViewController setToRecipients:[NSArray arrayWithObject:kFeedbackEmail]];
         [mailViewController setSubject:@"Feedback"];
         
-        
-        size_t size;
-        sysctlbyname("hw.machine", NULL, &size, NULL, 0);
-        char *machine = malloc(size);
-        sysctlbyname("hw.machine", machine, &size, NULL, 0);
-        NSString *platform = [NSString stringWithCString:machine encoding:NSUTF8StringEncoding];
-        NSLog(@"iPhone Device%@",[self platformType:platform]);
-        free(machine);
-        
-        
-        
         //get device specs
-        UIDevice *currentDevice  = [UIDevice currentDevice];
-        NSString *model                = [self platformType:platform];
-        NSString *systemVersion    = [currentDevice systemVersion];
-        NSString *appVersion         = [[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString *)kCFBundleVersionKey];
-        NSString *country               = [[NSLocale currentLocale] localeIdentifier];
-        NSString *uuid                    = [currentDevice userUniqueIdentiferForUser:_authenticationManager.jiveUserId];
-        NSString * pbx                    = _authenticationManager.line.pbx.displayName;
-        NSString *user                    = _authenticationManager.line.pbx.user.jiveUserId;
-        NSString *line                    = _authenticationManager.line.extension;
+        UIDevice *currentDevice     = [UIDevice currentDevice];
+        NSString *model             = [currentDevice platformType];
+        NSString *systemVersion     = [currentDevice systemVersion];
+        NSString *appVersion        = [[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString *)kCFBundleVersionKey];
+        NSString *country           = [[NSLocale currentLocale] localeIdentifier];
+        NSString *uuid              = [currentDevice userUniqueIdentiferForUser:_authenticationManager.jiveUserId];
+        NSString * pbx              = _authenticationManager.line.pbx.displayName;
+        NSString *user              = _authenticationManager.line.pbx.user.jiveUserId;
+        NSString *line              = _authenticationManager.line.extension;
         
         NSString *bodyTemplate = [NSString stringWithFormat:kJCSettingsTableViewControllerFeebackMessage, model, systemVersion, appVersion, country, uuid, pbx, user, line];
         [mailViewController setMessageBody:bodyTemplate isHTML:YES];
         [self presentViewController:mailViewController animated:YES completion:nil];
     }
 }
-
-- (NSString *) platformType:(NSString *)platform
-{
-    if ([platform isEqualToString:@"iPhone1,1"])    return @"iPhone 1G";
-    if ([platform isEqualToString:@"iPhone1,2"])    return @"iPhone 3G";
-    if ([platform isEqualToString:@"iPhone2,1"])    return @"iPhone 3GS";
-    if ([platform isEqualToString:@"iPhone3,1"])    return @"iPhone 4";
-    if ([platform isEqualToString:@"iPhone3,3"])    return @"Verizon iPhone 4";
-    if ([platform isEqualToString:@"iPhone4,1"])    return @"iPhone 4S";
-    if ([platform isEqualToString:@"iPhone5,1"])    return @"iPhone 5 (GSM)";
-    if ([platform isEqualToString:@"iPhone5,2"])    return @"iPhone 5 (GSM+CDMA)";
-    if ([platform isEqualToString:@"iPhone5,3"])    return @"iPhone 5c (GSM)";
-    if ([platform isEqualToString:@"iPhone5,4"])    return @"iPhone 5c (GSM+CDMA)";
-    if ([platform isEqualToString:@"iPhone6,1"])    return @"iPhone 5s (GSM)";
-    if ([platform isEqualToString:@"iPhone6,2"])    return @"iPhone 5s (GSM+CDMA)";
-    if ([platform isEqualToString:@"iPhone7,2"])    return @"iPhone 6";
-    if ([platform isEqualToString:@"iPhone7,1"])    return @"iPhone 6 Plus";
-    if ([platform isEqualToString:@"iPod1,1"])      return @"iPod Touch 1G";
-    if ([platform isEqualToString:@"iPod2,1"])      return @"iPod Touch 2G";
-    if ([platform isEqualToString:@"iPod3,1"])      return @"iPod Touch 3G";
-    if ([platform isEqualToString:@"iPod4,1"])      return @"iPod Touch 4G";
-    if ([platform isEqualToString:@"iPod5,1"])      return @"iPod Touch 5G";
-    if ([platform isEqualToString:@"iPad1,1"])      return @"iPad";
-    if ([platform isEqualToString:@"iPad2,1"])      return @"iPad 2 (WiFi)";
-    if ([platform isEqualToString:@"iPad2,2"])      return @"iPad 2 (GSM)";
-    if ([platform isEqualToString:@"iPad2,3"])      return @"iPad 2 (CDMA)";
-    if ([platform isEqualToString:@"iPad2,4"])      return @"iPad 2 (WiFi)";
-    if ([platform isEqualToString:@"iPad2,5"])      return @"iPad Mini (WiFi)";
-    if ([platform isEqualToString:@"iPad2,6"])      return @"iPad Mini (GSM)";
-    if ([platform isEqualToString:@"iPad2,7"])      return @"iPad Mini (GSM+CDMA)";
-    if ([platform isEqualToString:@"iPad3,1"])      return @"iPad 3 (WiFi)";
-    if ([platform isEqualToString:@"iPad3,2"])      return @"iPad 3 (GSM+CDMA)";
-    if ([platform isEqualToString:@"iPad3,3"])      return @"iPad 3 (GSM)";
-    if ([platform isEqualToString:@"iPad3,4"])      return @"iPad 4 (WiFi)";
-    if ([platform isEqualToString:@"iPad3,5"])      return @"iPad 4 (GSM)";
-    if ([platform isEqualToString:@"iPad3,6"])      return @"iPad 4 (GSM+CDMA)";
-    if ([platform isEqualToString:@"iPad4,1"])      return @"iPad Air (WiFi)";
-    if ([platform isEqualToString:@"iPad4,2"])      return @"iPad Air (Cellular)";
-    if ([platform isEqualToString:@"iPad4,3"])      return @"iPad Air";
-    if ([platform isEqualToString:@"iPad4,4"])      return @"iPad Mini 2G (WiFi)";
-    if ([platform isEqualToString:@"iPad4,5"])      return @"iPad Mini 2G (Cellular)";
-    if ([platform isEqualToString:@"iPad4,6"])      return @"iPad Mini 2G";
-    if ([platform isEqualToString:@"i386"])         return @"Simulator";
-    if ([platform isEqualToString:@"x86_64"])       return @"Simulator";
-    return platform;
-}
-
 
 -(IBAction)logout:(id)sender
 {
