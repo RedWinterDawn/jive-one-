5e9f38879069792c5ac1ce9cbd28d971f706001d
Pete <plen@jive.com>
2015-02-16 10:17:25 -0700

    adding SMSClient

---
+++ JiveOne/JCSMSClient.h
@@ -0,0 +1,13 @@
+//
+//  JCSMSClient.h
+//  JiveOne
+//
+//  Created by P Leonard on 2/16/15.
+//  Copyright (c) 2015 Jive Communications, Inc. All rights reserved.
+//
+
+#import "JCClient.h"
+
+@interface JCSMSClient : JCClient
+
+@end
---
+++ JiveOne/JCSMSClient.m
@@ -0,0 +1,13 @@
+//
+//  JCSMSClient.m
+//  JiveOne
+//
+//  Created by P Leonard on 2/16/15.
+//  Copyright (c) 2015 Jive Communications, Inc. All rights reserved.
+//
+
+#import "JCSMSClient.h"
+
+@implementation JCSMSClient
+
+@end
