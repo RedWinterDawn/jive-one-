16d3a109a0ba27bd31a1c749c2b86f34849dee52
Robert Barclay <rbarclay@jive.com>
2015-03-24 14:02:50 -0600

    added DIDs to the debug view to see all the kids and their relationships.

---
+++ JiveOne/JCDebugDidsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JCDebugDidsTableViewController.h
+//  JiveOne
+//
+//  Created by Robert Barclay on 3/24/15.
+//  Copyright (c) 2015 Jive Communications, Inc. All rights reserved.
+//
+
+#import "JCFetchedResultsTableViewController.h"
+
+@interface JCDebugDIDsTableViewController : JCFetchedResultsTableViewController
+
+@end
