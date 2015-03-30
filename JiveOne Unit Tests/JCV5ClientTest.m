0263a4cbc34a03f1ec91ef58a9af1256e632a92b
Robert Barclay <rbarclay@jive.com>
2015-03-30 10:25:59 -0600

    Started building unit test cases

--- JiveOne Unit Tests/JCV5ClientTest.m
+++ JiveOne Unit Tests/JCV5ClientTest.m
@@ -6,12 +6,17 @@
 //  Copyright (c) 2015 Jive Communications, Inc. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import <XCTest/XCTest.h>
+#import "JCCoreDataBaseTestCase.h"
+#import "JCV5ApiClient.h"
+#import "JCUnknownNumber.h"
 
-@interface JCV5ClientTest : XCTestCase
+@interface JCV5ClientTest : JCCoreDataBaseTestCase
 {
+    NSString *_authToken;
+    User *_user;
+    DID *_did;
     
+    JCUnknownNumber *_person;
 }
 
 @end
@@ -21,20 +26,57 @@
 - (void)setUp {
     [super setUp];
     
+    _authToken = @"";
     
+    // User
+    _user = [User MR_createInContext:self.context];
+    _user.jiveUserId = @"jivetesting11@gmail.com";
     
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    // DID
+    _did = [DID MR_createInContext:self.context];
+    _did.jrn = @"";
+    _did.number = @"";
+    
+    _person = [JCUnknownNumber new];
+    _person.name = @"Test Name";
+    _person.number = @"12345";
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
 
-- (void)testExample {
-    // This is an example of a functional test case.
+- (void)test_pbxInfo_successfull_download {
+    
+    // Given
+    
+    // When
+    [JCV5ApiClient requestPBXInforForUser:_user competion:^(BOOL success, id response, NSError *error) {
+        
+    }];
+    
+    // This
     XCTAssert(YES, @"Pass");
 }
+
+-(void)test_sms_messages_conversations_download {
+    
+    [JCV5ApiClient downloadMessagesDigestForDID:_did completion:^(BOOL success, id response, NSError *error) {
+        
+    }];
+}
+
+-(void)test_sms_messages_download {
+    
+    [JCV5ApiClient downloadMessagesForDID:_did completion:^(BOOL success, id response, NSError *error) {
+        
+    }];
+}
+
+-(void)test_sms_messages_conversation_download {
+    
+    [JCV5ApiClient downloadMessagesForDID:_did toPerson:_person completion:^(BOOL success, id response, NSError *error) {
+        
+    }];
+}
+
 
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
