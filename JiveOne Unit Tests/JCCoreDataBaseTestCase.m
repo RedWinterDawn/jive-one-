65ae85a124f3d80684955c38d1781879ab2ae95c
Robert Barclay <rbarclay@jive.com>
2015-03-30 10:25:59 -0600

    Built a core data base test case

---
+++ JiveOne Unit Tests/JCCoreDataBaseTestCase.m
@@ -0,0 +1,32 @@
+//
+//  JCCoreDataBaseTestCase.m
+//  JiveOne
+//
+//  Created by Robert Barclay on 3/27/15.
+//  Copyright (c) 2015 Jive Communications, Inc. All rights reserved.
+//
+
+#import "JCCoreDataBaseTestCase.h"
+
+@implementation JCCoreDataBaseTestCase
+
+- (void)setUp {
+    [super setUp];
+    
+    // Setup the default model from the current class' bundle
+    [MagicalRecord setDefaultModelFromClass:[self class]];
+    
+    // Setup a default in-memory store
+    [MagicalRecord setupCoreDataStackWithInMemoryStore];
+    
+    _context = [NSManagedObjectContext MR_defaultContext];
+}
+
+- (void)tearDown {
+    
+    [MagicalRecord cleanUp];
+    
+    [super tearDown];
+}
+
+@end
