7222d503bb19fbad293fa0075d751f305bc92d43
Robert Barclay <rbarclay@jive.com>
2015-04-06 12:38:31 -0600

    Defined a few test case classes.

---
+++ UnitTests/JCContactsTests.m
@@ -0,0 +1,18 @@
+//
+//  JCContactsTests.m
+//  JiveOne
+//
+//  Created by Robert Barclay on 4/6/15.
+//  Copyright (c) 2015 Jive Communications, Inc. All rights reserved.
+//
+
+#import "JCBaseTestCase.h"
+
+@interface JCContactsTests : JCBaseTestCase
+
+@end
+
+@implementation JCContactsTests
+
+
+@end
---
+++ UnitTests/JCPbxTests.m
@@ -0,0 +1,18 @@
+//
+//  JCPbxTests.m
+//  JiveOne
+//
+//  Created by Robert Barclay on 4/6/15.
+//  Copyright (c) 2015 Jive Communications, Inc. All rights reserved.
+//
+
+#import "JCBaseTestCase.h"
+
+@interface JCPbxTests : JCBaseTestCase
+
+@end
+
+@implementation JCPbxTests
+
+
+@end
