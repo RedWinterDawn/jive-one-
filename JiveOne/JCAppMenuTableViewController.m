8027bb9094c097402011d1129fd649cb19e686fb
Robert Barclay <rbarclay@jive.com>
2015-04-24 19:18:33 -0600

    integrated the new property from the app settings. also migrated some of the delegate to handle dependency injection.

--- JiveOne/JCAppMenuTableViewController.m
+++ JiveOne/JCAppMenuTableViewController.m
@@ -7,10 +7,24 @@
 //
 
 #import "JCAppMenuTableViewController.h"
+
+// View Controllers
 #import "JCStoryboardLoaderViewController.h"
 
+// Managers
 #import "JCPhoneManager.h"
+
+// Managed objects
 #import "PBX.h"
+
+// Objects
+#import "JCAppSettings.h"
+
+NSString *const kApplicationSwitcherPhoneIdentifier      = @"Phone";
+NSString *const kApplicationSwitcherMessagesIdentifier   = @"Messages";
+NSString *const kApplicationSwitcherContactsIdentifier   = @"Contacts";
+NSString *const kApplicationSwitcherSettingsIdentifier   = @"Settings";
+
 
 @implementation JCAppMenuTableViewController
 
@@ -19,9 +33,36 @@
     [super viewDidLoad];
     
     NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
-    [center addObserver:self selector:@selector(lineChanged:) name:kJCAuthenticationManagerLineChangedNotification object:self.authenticationManager];
     
+    JCAuthenticationManager *authenticationManager = self.authenticationManager;
     
+    [center addObserver:self selector:@selector(reset:) name:kJCAuthenticationManagerUserLoggedOutNotification object:authenticationManager];
+    [center addObserver:self selector:@selector(reload:) name:kJCAuthenticationManagerUserLoadedMinimumDataNotification object:authenticationManager];
+    [center addObserver:self selector:@selector(reload:) name:kJCAuthenticationManagerLineChangedNotification object:authenticationManager];
+    [center addObserver:self selector:@selector(showCall:) name:kJCPhoneManagerShowCallsNotification object:self.phoneManager];
+    
+    [self performSelectorOnMainThread:@selector(loadLastSelected) withObject:nil waitUntilDone:NO];
+}
+
+-(void)loadLastSelected
+{
+    NSString *identifier = self.appSettings.appSwitcherLastSelectedViewControllerIdentifier;
+    if(!identifier)
+        identifier = [self identifierForTableViewCell:self.phoneCell];
+    [self selectCellUsingIdentifier:identifier];
+}
+
+-(void)selectCellUsingIdentifier:(NSString *)identifier
+{
+    if (!identifier) {
+        return;
+    }
+    
+    UITableView *tableView = self.tableView;
+    UITableViewCell *tableViewCell = [self cellForIdentifier:identifier];
+    NSIndexPath *indexPath = [tableView indexPathForCell:tableViewCell];
+    [self.tableView selectRowAtIndexPath:indexPath animated:NO scrollPosition:UITableViewScrollPositionNone];
+    [self performSegueWithIdentifier:identifier sender:tableViewCell];
 }
 
 - (void)dealloc
@@ -50,13 +91,71 @@
     }
 }
 
--(void)lineChanged:(NSNotification *)notification
+#pragma mark - Notification Handlers -
+
+-(void)reload:(NSNotification *)notification
 {
     [self cell:self.messageCell setHidden:!self.authenticationManager.pbx.smsEnabled];
     [self reloadDataAnimated:NO];
     
     CGSize size = self.tableView.contentSize;
     [_delegate appMenuTableViewController:self willChangeToSize:size];
+}
+
+-(void)reset:(NSNotification *)notification
+{
+    self.appSettings.appSwitcherLastSelectedViewControllerIdentifier = nil;
+}
+
+-(void)showCall:(NSNotification *)notification
+{
+    UITableView *tableView = self.tableView;
+    NSIndexPath *indexPath = [tableView indexPathForSelectedRow];
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    UITableViewCell *phoneCell = self.phoneCell;
+    
+    // If we are currently on that cell, we do not need to change to that cell, so exit.
+    if ([cell isEqual:phoneCell]) {
+        return;
+    }
+    
+    [self selectCellUsingIdentifier:kApplicationSwitcherPhoneIdentifier];
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    NSString *identifier = [self identifierForTableViewCell:cell];
+    self.appSettings.appSwitcherLastSelectedViewControllerIdentifier = identifier;
+}
+
+-(NSString *)identifierForTableViewCell:(UITableViewCell *)cell
+{
+    if ([cell isEqual:self.settingsViewCell]) {
+        return kApplicationSwitcherSettingsIdentifier;
+    }
+    else if([cell isEqual:self.messageCell]) {
+        return kApplicationSwitcherMessagesIdentifier;
+    }
+    else if ([cell isEqual:self.contactsCell]) {
+        return kApplicationSwitcherContactsIdentifier;
+    }
+    else {
+        return kApplicationSwitcherPhoneIdentifier;
+    }
+}
+
+-(UITableViewCell *)cellForIdentifier:(NSString *)identifier
+{
+    if ([identifier isEqualToString:kApplicationSwitcherSettingsIdentifier]) {
+        return self.settingsViewCell;
+    } else if ([identifier isEqualToString:kApplicationSwitcherMessagesIdentifier]) {
+        return self.messageCell;
+    } else if ([identifier isEqualToString:kApplicationSwitcherContactsIdentifier]) {
+        return self.contactsCell;
+    } else {
+        return self.phoneCell;
+    }
 }
 
 @end
