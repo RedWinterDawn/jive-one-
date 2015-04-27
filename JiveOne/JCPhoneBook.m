6d12fd2a91678f55fa00622f9260d4c6b777f274
Robert Barclay <rbarclay@jive.com>
2015-04-27 15:24:21 -0600

    fixed when dialing *99 that it would call a random contact.

--- JiveOne/JCPhoneBook.m
+++ JiveOne/JCPhoneBook.m
@@ -14,6 +14,7 @@
 #import "PBX.h"
 #import "LocalContact.h"
 #import "Extension.h"
+#import "JCVoicemailNumber.h"
 
 @interface JCPhoneBook () {
     JCAddressBook *_addressBook;
@@ -49,6 +50,10 @@
     // We must at least have a number. If we do not have a number, we return nil.
     if (!number) {
         return nil;
+    }
+    
+    if ([number isEqual:@"*99"]) {
+        return [JCVoicemailNumber new];
     }
     
     // Check if the number is the extension for a jive contact. Since extensions are unique to a
