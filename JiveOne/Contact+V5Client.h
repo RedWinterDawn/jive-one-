f77bedcd7f2cf5027e858ad1a6317c69111e6162
Robert Barclay <rbarclay@jive.com>
2015-06-15 15:48:05 -0600

    Started building the Contacts V5 Client.

---
+++ JiveOne/Contact+V5Client.h
@@ -0,0 +1,13 @@
+//
+//  Contact+V5Client.h
+//  JiveOne
+//
+//  Created by Robert Barclay on 6/15/15.
+//  Copyright (c) 2015 Jive Communications, Inc. All rights reserved.
+//
+
+#import "Contact.h"
+
+@interface Contact (V5Client)
+
+@end
---
+++ JiveOne/Contact+V5Client.m
@@ -0,0 +1,13 @@
+//
+//  Contact+V5Client.m
+//  JiveOne
+//
+//  Created by Robert Barclay on 6/15/15.
+//  Copyright (c) 2015 Jive Communications, Inc. All rights reserved.
+//
+
+#import "Contact+V5Client.h"
+
+@implementation Contact (V5Client)
+
+@end
