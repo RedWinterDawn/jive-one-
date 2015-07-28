10c0644d68c848f5dc39084d400c0f1bc0a47cf0
Robert Barclay <rbarclay@jive.com>
2015-07-28 17:20:38 -0600

    Defined an JCFetchedResultsController superclass object.

---
+++ JiveOne/JCFetchedResultsController.h
@@ -0,0 +1,13 @@
+//
+//  JCFetchedResultsController.h
+//  JiveOne
+//
+//  Created by Robert Barclay on 7/27/15.
+//  Copyright (c) 2015 Jive Communications, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JCFetchedResultsController : NSObject
+
+@end
---
+++ JiveOne/JCFetchedResultsController.m
@@ -0,0 +1,13 @@
+//
+//  JCFetchedResultsController.m
+//  JiveOne
+//
+//  Created by Robert Barclay on 7/27/15.
+//  Copyright (c) 2015 Jive Communications, Inc. All rights reserved.
+//
+
+#import "JCFetchedResultsController.h"
+
+@implementation JCFetchedResultsController
+
+@end
